65bf978bb661ae3525b650e3b9d222ab08bb41ea
Patrick Reynolds <cpreynolds@gmail.com>
2013-08-14 21:38:37 -0400

    Fixing requirements order and adding some mocks for doc build.

--- docs/conf.py
+++ docs/conf.py
@@ -18,6 +18,30 @@ import sys, os
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('..'))
 
+# Creating mock imports so that readthedocs works even though building
+# bcrypt fails
+class Mock(object):
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def __call__(self, *args, **kwargs):
+        return Mock()
+
+    @classmethod
+    def __getattr__(cls, name):
+        if name in ('__file__', '__path__'):
+            return '/dev/null'
+        elif name[0] == name[0].upper():
+            mockType = type(name, (), {})
+            mockType.__module__ = __name__
+            return mockType
+        else:
+            return Mock()
+
+MOCK_MODULES = ['bcrypt']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = Mock()
+
 # -- General configuration -----------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
