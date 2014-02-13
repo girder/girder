340eb8e30bc2169085bda2db91b01cb5461300d8
Zach Mullen <zach.mullen@kitware.com>
2014-02-12 21:18:55 -0500

    Add routes for specific folders in the hierarchy widget

--- clients/web/src/views/body/CollectionView.js
+++ clients/web/src/views/body/CollectionView.js
@@ -1,101 +1,138 @@
-/**
- * This view shows a single collection's page.
- */
-girder.views.CollectionView = Backbone.View.extend({
-    events: {
-        'click .g-edit-collection': 'editCollection',
-        'click .g-collection-access-control': 'editAccess'
-    },
+(function () {
+    /**
+     * This view shows a single collection's page.
+     */
+    girder.views.CollectionView = Backbone.View.extend({
+        events: {
+            'click .g-edit-collection': 'editCollection',
+            'click .g-collection-access-control': 'editAccess'
+        },
 
-    initialize: function (settings) {
-        // If collection model is already passed, there is no need to fetch.
-        if (settings.collection) {
-            this.model = settings.collection;
-            this.render();
+        initialize: function (settings) {
+            // If collection model is already passed, there is no need to fetch.
+            if (settings.collection) {
+                this.model = settings.collection;
 
-            // This page should be re-rendered if the user logs in or out
-            girder.events.on('g:login', this.userChanged, this);
-        }
-        else {
-            console.error('Implement fetch then render collection');
-        }
+                if (settings.folderId) {
+                    this.folder = new girder.models.FolderModel();
+                    this.folder.set({
+                        _id: settings.folderId
+                    }).on('g:fetched', function () {
+                        this.render();
+                    }, this).on('g:error', function () {
+                        this.folder = null;
+                        this.render();
+                    }, this).fetch();
+                }
+                else {
+                    this.render();
+                }
 
-    },
+                // This page should be re-rendered if the user logs in or out
+                girder.events.on('g:login', this.userChanged, this);
+            }
+            else {
+                console.error('Implement fetch then render collection');
+            }
 
-    editCollection: function () {
-        var container = $('#g-dialog-container');
+        },
 
-        if (!this.editCollectionWidget) {
-            this.editCollectionWidget = new girder.views.EditCollectionWidget({
-                el: container,
-                model: this.model
-            }).off('g:saved').on('g:saved', function (collection) {
+        editCollection: function () {
+            var container = $('#g-dialog-container');
+
+            if (!this.editCollectionWidget) {
+                this.editCollectionWidget = new girder.views.EditCollectionWidget({
+                    el: container,
+                    model: this.model
+                }).off('g:saved').on('g:saved', function (collection) {
+                    this.render();
+                }, this);
+            }
+            this.editCollectionWidget.render();
+        },
+
+        render: function () {
+            this.$el.html(jade.templates.collectionPage({
+                collection: this.model,
+                girder: girder
+            }));
+
+            this.hierarchyWidget = new girder.views.HierarchyWidget({
+                parentType: this.folder ? 'folder' : 'collection',
+                parentModel: this.folder || this.model,
+                el: this.$('.g-collection-hierarchy-container')
+            });
+
+            this.$('.g-collection-actions-button').tooltip({
+                container: 'body',
+                placement: 'left',
+                animation: false,
+                delay: {show: 100}
+            });
+
+            if (this.folder) {
+                girder.router.navigate('collection/' + this.model.get('_id') + '/folder/'
+                    + this.folder.get('_id'));
+            }
+            else {
+                girder.router.navigate('collection/' + this.model.get('_id'));
+            }
+
+            return this;
+        },
+
+        userChanged: function () {
+            // When the user changes, we should refresh the model to update the
+            // _accessLevel attribute on the viewed collection, then re-render the
+            // page.
+            this.model.off('g:fetched').on('g:fetched', function () {
                 this.render();
+            }, this).on('g:error', function () {
+                // Current user no longer has read access to this user, so we
+                // send them back to the user list page.
+                girder.events.trigger('g:navigateTo',
+                    girder.views.CollectionsView);
+            }, this).fetch();
+        },
+
+        editAccess: function () {
+            new girder.views.AccessWidget({
+                el: $('#g-dialog-container'),
+                modelType: 'collection',
+                model: this.model
+            }).on('g:saved', function (collection) {
+                // need to do anything?
             }, this);
         }
-        this.editCollectionWidget.render();
-    },
 
-    render: function () {
-        this.$el.html(jade.templates.collectionPage({
-            collection: this.model,
-            girder: girder
-        }));
+    });
 
-        this.hierarchyWidget = new girder.views.HierarchyWidget({
-            parentType: 'collection',
-            parentModel: this.model,
-            el: this.$('.g-collection-hierarchy-container')
-        });
-
-        this.$('.g-collection-actions-button').tooltip({
-            container: 'body',
-            placement: 'left',
-            animation: false,
-            delay: {show: 100}
-        });
-
-        girder.router.navigate('collection/' + this.model.get('_id'));
-
-        return this;
-    },
-
-    userChanged: function () {
-        // When the user changes, we should refresh the model to update the
-        // _accessLevel attribute on the viewed collection, then re-render the
-        // page.
-        this.model.off('g:fetched').on('g:fetched', function () {
-            this.render();
+    /**
+     * Helper function for fetching the user and rendering the view with
+     * an arbitrary set of extra parameters.
+     */
+    var _fetchAndInit = function (collectionId, params) {
+        var collection = new girder.models.CollectionModel();
+        collection.set({
+            _id: collectionId
+        }).on('g:fetched', function () {
+            girder.events.trigger('g:navigateTo', girder.views.CollectionView, _.extend({
+                collection: collection
+            }, params || {}));
         }, this).on('g:error', function () {
-            // Current user no longer has read access to this user, so we
-            // send them back to the user list page.
-            girder.events.trigger('g:navigateTo',
-                girder.views.CollectionsView);
+            girder.events.trigger('g:navigateTo', girder.views.CollectionsView);
         }, this).fetch();
-    },
+    };
 
-    editAccess: function () {
-        new girder.views.AccessWidget({
-            el: $('#g-dialog-container'),
-            modelType: 'collection',
-            model: this.model
-        }).on('g:saved', function (collection) {
-            // need to do anything?
-        }, this);
-    }
+    girder.router.route('collection/:id', 'collection', function (collectionId) {
+        _fetchAndInit(collectionId);
+    });
 
-});
+    girder.router.route('collection/:id/folder/:id', 'collectionFolder',
+        function (collectionId, folderId) {
+            _fetchAndInit(collectionId, {
+                folderId: folderId
+            });
+        });
 
-girder.router.route('collection/:id', 'collection', function (id) {
-    // Fetch the collection by id, then render the view.
-    var collection = new girder.models.CollectionModel();
-    collection.set({
-        _id: id
-    }).on('g:fetched', function () {
-        girder.events.trigger('g:navigateTo', girder.views.CollectionView, {
-            collection: collection
-        }, collection);
-    }, this).on('g:error', function () {
-        girder.events.trigger('g:navigateTo', girder.views.CollectionsView);
-    }, this).fetch();
-});
+}) ();
--- clients/web/src/views/body/UserView.js
+++ clients/web/src/views/body/UserView.js
@@ -1,63 +1,98 @@
-/**
- * This view shows a single user's page.
- */
-girder.views.UserView = Backbone.View.extend({
-    events: {
-    },
+(function () {
 
-    initialize: function (settings) {
-        // If user model is already passed, there is no need to fetch.
-        if (settings.user) {
-            this.model = settings.user;
-            this.render();
+    /**
+     * This view shows a single user's page.
+     */
+    girder.views.UserView = Backbone.View.extend({
+        initialize: function (settings) {
+            this.folderId = settings.folderId || null;
+
+            if (settings.user) {
+                this.model = settings.user;
+
+                if (settings.folderId) {
+                    this.folder = new girder.models.FolderModel();
+                    this.folder.set({
+                        _id: settings.folderId
+                    }).on('g:fetched', function () {
+                        this.render();
+                    }, this).on('g:error', function () {
+                        this.folder = null;
+                        this.render();
+                    }, this).fetch();
+                }
+                else {
+                    this.render();
+                }
+            }
+            else {
+                console.error('Implement fetch then render user');
+            }
+
+            // This page should be re-rendered if the user logs in or out
+            girder.events.on('g:login', this.userChanged, this);
+        },
+
+        render: function () {
+            this.$el.html(jade.templates.userPage({
+                user: this.model
+            }));
+
+            this.hierarchyWidget = new girder.views.HierarchyWidget({
+                parentType: this.folder ? 'folder' : 'user',
+                parentModel: this.folder || this.model,
+                el: this.$('.g-user-hierarchy-container')
+            });
+
+            if (this.folder) {
+                girder.router.navigate('user/' + this.model.get('_id') + '/folder/'
+                    + this.folder.get('_id'));
+            }
+            else {
+                girder.router.navigate('user/' + this.model.get('_id'));
+            }
+
+            return this;
+        },
+
+        userChanged: function () {
+            // When the user changes, we should refresh the model to update the
+            // accessLevel attribute on the viewed user, then re-render the page.
+            this.model.off('g:fetched').on('g:fetched', function () {
+                this.render();
+            }, this).on('g:error', function () {
+                // Current user no longer has read access to this user, so we
+                // send them back to the user list page.
+                girder.events.trigger('g:navigateTo', girder.views.UsersView);
+            }, this).fetch();
         }
-        else {
-            console.error('Implement fetch then render user');
-        }
+    });
 
-        // This page should be re-rendered if the user logs in or out
-        girder.events.on('g:login', this.userChanged, this);
-    },
-
-    render: function () {
-        this.$el.html(jade.templates.userPage({
-            user: this.model
-        }));
-
-        this.hierarchyWidget = new girder.views.HierarchyWidget({
-            parentType: 'user',
-            parentModel: this.model,
-            el: this.$('.g-user-hierarchy-container')
-        });
-
-        girder.router.navigate('user/' + this.model.get('_id'));
-
-        return this;
-    },
-
-    userChanged: function () {
-        // When the user changes, we should refresh the model to update the
-        // accessLevel attribute on the viewed user, then re-render the page.
-        this.model.off('g:fetched').on('g:fetched', function () {
-            this.render();
+    /**
+     * Helper function for fetching the user and rendering the view with
+     * an arbitrary set of extra parameters.
+     */
+    var _fetchAndInit = function (userId, params) {
+        var user = new girder.models.UserModel();
+        user.set({
+            _id: userId
+        }).on('g:fetched', function () {
+            girder.events.trigger('g:navigateTo', girder.views.UserView, _.extend({
+                user: user
+            }, params || {}));
         }, this).on('g:error', function () {
-            // Current user no longer has read access to this user, so we
-            // send them back to the user list page.
             girder.events.trigger('g:navigateTo', girder.views.UsersView);
         }, this).fetch();
-    }
-});
+    };
 
-girder.router.route('user/:id', 'user', function (id) {
-    // Fetch the user by id, then render the view.
-    var user = new girder.models.UserModel();
-    user.set({
-        _id: id
-    }).on('g:fetched', function () {
-        girder.events.trigger('g:navigateTo', girder.views.UserView, {
-            user: user
-        }, user);
-    }, this).on('g:error', function () {
-        girder.events.trigger('g:navigateTo', girder.views.UsersView);
-    }, this).fetch();
-});
+    girder.router.route('user/:id', 'user', function (userId) {
+        _fetchAndInit(userId);
+    });
+
+    girder.router.route('user/:id/folder/:id', 'userFolder', function (userId, folderId) {
+        _fetchAndInit(userId, {
+            folderId: folderId
+        });
+    });
+
+}) ();
