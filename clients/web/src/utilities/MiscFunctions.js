6affe9dd43abbe8c95326690c58fb04087407d7e
Patrick Reynolds <cpreynolds@gmail.com>
2014-05-19 14:33:33 -0400

    Adding a function for parsing query strings.

--- clients/web/src/utilities/MiscFunctions.js
+++ clients/web/src/utilities/MiscFunctions.js
@@ -111,3 +111,25 @@ girder.getModelClassByName = function (name) {
     var className = name.charAt(0).toUpperCase();
     return className + name.substr(1) + 'Model';
 };
+
+girder.parseQueryString = function (queryString) {
+    var params = {};
+    if (queryString) {
+        _.each(
+            _.map(decodeURI(queryString).split(/&/g),function (el, i) {
+                var aux = el.split('='), o = {};
+                if (aux.length >= 1) {
+                    var val;
+                    if(aux.length == 2)
+                        val = aux[1];
+                    o[aux[0]] = val;
+                }
+                return o;
+            }),
+            function (o) {
+                _.extend(params,o);
+            }
+        );
+    }
+    return params;
+};
