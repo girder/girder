cc67e6aad72a763da96b2a9649f159fd7bd86e0b
Zach Mullen <zach.mullen@kitware.com>
2014-05-24 23:10:50 -0400

    Output coverage data from blanket in cobertura formatted XML

--- clients/web/test/coverageHandler.js
+++ clients/web/test/coverageHandler.js
@@ -14,83 +14,103 @@
  * handleCoverage() returns true in the case of success, and false otherwise.
  */
 window.coverageHandler = (function () {
+    /**
+     * Helper to get the coverage results for an individual file.
+     * @param data The per-file value from blanket
+     * @param overall The object to write overall coverage data back to.
+     */
+    var _getDataForFile = function (data, overall) {
+        var ret = {
+            hits: 0,
+            sloc: 0,
+            lines: {}
+        };
+        data.source.forEach(function (line, num) {
+            num++;
+            var coverageLine;
+            if (data[num] === 0) {
+                ret.sloc++;
+                ret.lines[num] = 0;
+            } else if (data[num] !== undefined) {
+                ret.hits++;
+                ret.sloc++;
+                ret.lines[num] = 1;
+            }
+        });
+        ret.coverage = ret.sloc > 0 ? (ret.hits / ret.sloc) : 0;
+
+        overall.hits += ret.hits;
+        overall.sloc += ret.sloc;
+
+        return ret;
+    };
+
+    /**
+     * Writes the coverage output as a cobertura-compliant XML file. Flushes
+     * the output in chunks to the console with the expecation that it will
+     * be consumed and written to a file by the containing environment.
+     * @param perFile The perfile data built by _getDataForFile.
+     * @param overall The overage coverage values.
+     */
+    var _writeOutputXml = function (perFile, overall) {
+        console.log('<?xml version="1.0" ?>\n<!DOCTYPE coverage SYSTEM ' +
+            '\'http://cobertura.sourceforge.net/xml/coverage-03.dtd\'>\n');
+        console.log('<coverage branch-rate="0" line-rate="' + overall.coverage +
+            '" timestamp="' + new Date().getTime() + '" version="3.6">\n' +
+            '\t<packages>\n\t\t<package name="" branch-rate="0" complexity="0"' +
+            'line-rate="' + overall.coverage + '">\n\t\t\t<classes>\n');
+
+        _.each(perFile, function (data, filename) {
+            var fileXml = '\t\t\t\t<class branch-rate="0" complexity="0" ' +
+                'filename="' + filename + '" line-rate="' + data.coverage.toFixed(3) +
+                '" name="' + filename + '">\n\t\t\t\t\t<methods />\n' +
+                '\t\t\t\t\t<lines>\n';
+
+            _.each(data.lines, function (val, lineNum) {
+                fileXml += '\t\t\t\t\t\t<line hits="' + val + '" number="' +
+                    lineNum + '" />\n';
+            });
+
+            fileXml += '\t\t\t\t\t</lines>\n\t\t\t\t</class>\n';
+            console.log(fileXml);
+        });
+
+        console.log('\t\t\t</classes>\n\t\t</package>\n\t</packages>\n</coverage>\n');
+    };
+
     var publicApi = {
         handleCoverage: function (cov, userOptions) {
             if (!cov) {
                 return false;
             }
 
+            // Default option values
             var options = {
-                threshold: 50 //defaults to 50%
+                threshold: 50
             };
 
+            // User-specified option overrides
             if (userOptions) {
                 options.threshold = userOptions.threshold || options.threshold;
             }
 
-            var totals = [];
-            for (var filename in cov) {
-                console.log(filename);
-                      var data = cov[filename];
-                var reportData = this.reportFile(data, options);
-                var linesCovered = reportData[0];
-                var linesTotal = reportData[1];
-                var percentCovered = reportData[2];
-                console.log(linesCovered +" / "+linesTotal +" lines covered; "+percentCovered+"% coverage");
-                totals.push(reportData);
-            }
-
-            var totalHits = 0;
-            var totalSloc = 0;
-            totals.forEach(function(elem){
-              totalHits += elem[0];
-              totalSloc += elem[1];
-            });
-
-            var globCoverage = (totalHits === 0 || totalSloc === 0) ?
-                                  0 : totalHits / totalSloc * 100;
-            console.log("Coverage: "+Math.floor(globCoverage)+"%");
-
-            if (globCoverage < options.threshold || isNaN(globCoverage)){
-              console.error("Code coverage below threshold: "+Math.floor(globCoverage)+ " < "+options.threshold);
-              if (typeof process !== "undefined"){
-                process.exit(1);
-              }
-              return false;
-
-            }else{
-              console.log("Coverage succeeded.");
-            }
-            return true;
-          },
-          reportFile: function( data,options) {
-            var ret = {
-              coverage: 0,
-              hits: 0,
-              misses: 0,
-              sloc: 0
+            var perFile = {};
+            var overall = {
+                hits: 0,
+                sloc: 0
             };
-            data.source.forEach(function(line, num){
-              num++;
-              var coverageLine;
-              if (data[num] === 0) {
-                ret.misses++;
-                ret.sloc++;
-                coverageLine = "0";
-              } else if (data[num] !== undefined) {
-                ret.hits++;
-                ret.sloc++;
-                coverageLine = data[num];
-              } else {
-                coverageLine = 'U';
-              }
-              coverageLine = coverageLine + " " + line;
-              console.log(coverageLine);
-            });
-            ret.coverage = ret.hits / ret.sloc * 100;
 
-            return [ret.hits,ret.sloc,ret.coverage];
-          }
+            _.each(cov, function(data, filename) {
+                perFile[filename] = _getDataForFile(data, overall);
+            });
+
+            overall.coverage = overall.sloc > 0 ?
+                (overall.hits / overall.sloc) : 0;
+
+            _writeOutputXml(perFile, overall);
+
+            return overall.coverage * 100 > options.threshold;
+        }
     };
     return publicApi;
 })();
