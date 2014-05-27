dbba11bb26cf458dc12dbd79a07817e83e8bef15
Zach Mullen <zach.mullen@kitware.com>
2014-05-27 16:04:23 -0400

    Actually turn on coverage for travis continuous builds

--- cmake/travis_build.cmake
+++ cmake/travis_build.cmake
@@ -6,12 +6,16 @@ set(CTEST_SITE "Travis")
 set(CTEST_BUILD_NAME "Linux-$ENV{TRAVIS_BRANCH}")
 set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
 
+file(WRITE "${CTEST_BINARY_DIRECTORY}/CMakeCache.txt" "
+PYTHON_COVERAGE:BOOL=ON
+")
+
 ctest_start("Continuous")
 ctest_configure()
 ctest_build()
 ctest_test(PARALLEL_LEVEL 3 RETURN_VALUE res)
 ctest_coverage()
-file(REMOVE ${CTEST_BINARY_DIRECTORY}/coverage.xml)
+file(REMOVE "${CTEST_BINARY_DIRECTORY}/coverage.xml")
 
 if(NOT res EQUAL 0)
   message(FATAL_ERROR "Test failures occurred.")
