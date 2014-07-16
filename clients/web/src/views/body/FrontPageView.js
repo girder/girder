c426f7ad1393c9b51cb6991df5aae275966e21d3
Zach Mullen <zach.mullen@kitware.com>
2014-07-16 19:44:29 -0400

    Add link in single-page app to view the swagger page

--- clients/web/src/views/body/FrontPageView.js
+++ clients/web/src/views/body/FrontPageView.js
@@ -30,6 +30,7 @@ girder.views.FrontPageView = girder.View.extend({
 
     render: function () {
         this.$el.html(jade.templates.frontPage({
+            apiRoot: girder.apiRoot,
             staticRoot: girder.staticRoot,
             currentUser: girder.currentUser
         }));
