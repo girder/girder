d6999c2449f83950b5db7d4f3aaf8f91df1df5f7
Patrick Reynolds <patrick.reynolds@kitware.com>
2014-08-30 10:25:07 -0400

    Adding the first pass at a Google Analytics plugin
    This includes the backbone.analytics.js file as well as JS and Python style
    tests. The server component of the plugin is only for validation of the plugin
    setting.

---
+++ plugins/google_analytics/server/constants.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+###############################################################################
+#  Copyright Kitware Inc.
+#
+#  Licensed under the Apache License, Version 2.0 ( the "License" );
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+###############################################################################
+
+
+# Constants representing the setting keys for this plugin
+class PluginSettings:
+    GOOGLE_ANALYTICS_TRACKING_ID = 'google_analytics.tracking_id'
