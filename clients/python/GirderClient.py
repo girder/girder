e836b59e82d09e4f285d2854d95a71fec78f4c02
Jamie Snape <jamie.snape@kitware.com>
2014-09-03 15:46:43 -0400

    Minor cleanup of Python client

--- clients/python/GirderClient.py
+++ clients/python/GirderClient.py
@@ -5,23 +5,25 @@ import os
 
 class GirderClient(object):
     """
-    A class for interacting with the Girder RESTful api.  Some simple examples
-    of how to use this class follow:
+    A class for interacting with the Girder RESTful API. Some simple examples of
+    how to use this class follow:
 
-    c = GirderClient('myhost', 9000)
-    c.authenticate('myname', 'mypass')
+    client = GirderClient('myhost', 9000)
+    client.authenticate('myname', 'mypass')
 
-    itemId = c.createitem(folderId, 'some item name', 'and description')
-    c.addMetaDataToItem(itemId, {'metakey': 'metavalue'})
-    c.uploadFileToItem(itemId, '/full/path/to/your/file.txt')
+    itemId = client.createitem(folderId, 'some item name', 'and description')
+    client.addMetadataToItem(itemId, {'metadatakey': 'metadatavalue'})
+    client.uploadFileToItem(itemId, '/full/path/to/your/file.txt')
 
-    r1 = c.getItem('52e935037bee0436e29a7130')
-    r2 = c.sendRestRequest('GET', 'item', {'folderId': '52e97b2b7bee0436e29a7142', 'sortdir': '-1' })
-    r3 = c.sendRestRequest('GET', 'resource/search', {'q': 'aggregated', 'types': '["folder", "item"]'})
+    r1 = client.getItem('52e935037bee0436e29a7130')
+    r2 = client.sendRestRequest('GET', 'item',
+        {'folderId': '52e97b2b7bee0436e29a7142', 'sortdir': '-1' })
+    r3 = client.sendRestRequest('GET', 'resource/search',
+        {'q': 'aggregated','types': '["folder", "item"]'})
     """
 
-    # A convenience dictionary mapping method names to functions
-    # in the requests module.
+    # A convenience dictionary mapping HTTP method names to functions in the
+    # requests module
     METHODS = {
         'GET': requests.get,
         'POST': requests.post,
@@ -37,34 +39,36 @@ class GirderClient(object):
     #-------------------------------------------------------------------------
     def __init__(self, host="localhost", port=8080):
         """
-        Construct a new GirderClient object, given a hostname and
-        portnumber, as well as a username and password which will be used
-        in all requests (HTTP Basic Auth).
+        Construct a new GirderClient object, given a host name and port number,
+        as well as a username and password which will be used in all requests
+        (HTTP Basic Auth).
 
-            host: A string containing the hostname where girder is running,
-            the default value is 'localhost'.
+            host: A string containing the host name where Girder is running, the
+            default value is 'localhost'
 
-            port: A number containing the port on which to connect to girder,
-            the default value is 8080.
+            port: A number containing the port on which to connect to Girder,
+            the default value is 8080
         """
         self.host = host
         self.port = port
 
         self.urlBase = 'http://' + self.host + ':' + str(self.port) + '/api/v1/'
 
+        self.token = None
+
     #-------------------------------------------------------------------------
-    # Construct url and send request
+    # Construct URL and send request
     #-------------------------------------------------------------------------
     def authenticate(self, username, password):
         """
-        Authenticate to Girder, storing the token that comes back to be
-        used in future requests.
+        Authenticate to Girder, storing the token that comes back to be used in
+        future requests.
 
             username: A string containing the username to use in basic
-            authentication.
+            authentication
 
             password: A string containing the password to use in basic
-            authentication.
+            authentication
         """
         if username is None or password is None:
             raise Exception('A user name and password are required')
@@ -74,29 +78,27 @@ class GirderClient(object):
         self.token = authResponse.json()['authToken']['token']
 
     #-------------------------------------------------------------------------
-    # Construct url and send request
+    # Construct URL and send request
     #-------------------------------------------------------------------------
     def sendRestRequest(self, method, path, parameters=None):
         """
-        This method looks up the appropriate method, constructs a request
-        url from the base url, path, and parameters, and then sends the
-        request.  If the method is unknown or if the path is not found,
-        an exception is raised, otherwise a json object is returned with
-        the Girder response.
+        This method looks up the appropriate method, constructs a request URL
+        from the base URL, path, and parameters, and then sends the request. If
+        the method is unknown or if the path is not found, an exception is
+        raised, otherwise a JSON object is returned with the Girder response.
 
-        This is a convenience method to use when making basic requests
-        which do not involve multipart file data which might need to
-        be specially encoded or handled differently.
+        This is a convenience method to use when making basic requests that do
+        not involve multipart file data which might need to be specially encoded
+        or handled differently.
 
             method: One of 'GET', 'POST', 'PUT', or 'DELETE'
 
-            path: A string containing the path elements for this request.
-            Note that the path string should not begin or end with the
-            path separator, '/'.
+            path: A string containing the path elements for this request. Note
+            that the path string should not begin or end with the path
+            separator, '/'.
 
-            parameters: A dictionary mapping strings to strings, to be used
-            as the key/value pairs in the request parameters.
-
+            parameters: A dictionary mapping strings to strings, to be used as
+            the key/value pairs in the request parameters
         """
         if not parameters:
             parameters = {}
@@ -104,7 +106,7 @@ class GirderClient(object):
         # Make sure we got a valid method
         assert method in self.METHODS
 
-        # Look up the http method we need
+        # Look up the HTTP method we need
         f = self.METHODS[method]
 
         # Construct the url
@@ -122,14 +124,15 @@ class GirderClient(object):
         else:
             print 'Showing result before raising exception:'
             print result.text
-            raise Exception('Request: ' + result.url + ', return code: ' + str(result.status_code))
+            raise Exception('Request: ' + result.url + ', return code: ' +
+                            str(result.status_code))
 
     #-------------------------------------------------------------------------
-    # A convenience method for creating a new item.
+    # A convenience method for creating a new item
     #-------------------------------------------------------------------------
     def createItem(self, parentFolderId, name, description):
         """
-        Creates an item and returns the new item id.
+        Creates an item and returns the new item ID
         """
         path = 'item'
         params = {
@@ -141,10 +144,11 @@ class GirderClient(object):
         if '_id' in obj:
             return obj['_id']
         else:
-            raise Exception('Error, expected the returned item object to have an "_id" field')
+            raise Exception('Error, expected the returned item object to have'
+                            ' an "_id" field')
 
     #-------------------------------------------------------------------------
-    # A convenience method for creating a new folder.
+    # A convenience method for creating a new folder
     #-------------------------------------------------------------------------
     def createFolder(self, parentId, parentType, name, description):
         path = 'folder'
@@ -158,18 +162,19 @@ class GirderClient(object):
         if '_id' in obj:
             return obj['_id']
         else:
-            raise Exception('Error, expected to get a folder back with an "_id" field')
+            raise Exception('Error, expected to get a folder back with an "_id"'
+                            ' field')
 
     #-------------------------------------------------------------------------
     # A convenience method for adding metadata to an existing item
     #-------------------------------------------------------------------------
-    def addMetaDataToItem(self, itemId, metadata):
+    def addMetadataToItem(self, itemId, metadata):
         """
-        Takes an item id and a json object containing the metadata.
+        Takes an item ID and a JSON object containing the metadata
         """
         path = 'item/' + itemId + '/metadata'
-        #return self.sendRestRequest('PUT', path, params={'token': self.token}, data=metadata)
-        obj = requests.put(self.urlBase + path, params={'token': self.token}, data=json.dumps(metadata))
+        obj = requests.put(self.urlBase + path, params={'token': self.token},
+                           data=json.dumps(metadata))
         return obj.json()
 
     #-------------------------------------------------------------------------
@@ -177,12 +182,9 @@ class GirderClient(object):
     #-------------------------------------------------------------------------
     def uploadFileToItem(self, itemId, filepath):
         """
-        Uploads a file to an item.  Currently only supports uploading
-        in a single chunk, so files larger than 64 MB will raise an
-        exception.
+        Uploads a file to an item. Currently only supports uploading in a
+        single chunk, so files larger than 64 MB will raise an exception.
         """
-        data = None
-
         with open(filepath, 'rb') as fd:
             data = fd.read()
 
@@ -190,9 +192,9 @@ class GirderClient(object):
 
         if datalen > self.MAX_CHUNK_SIZE:
             raise Exception('Currently sending file larger than ' +
-                            str(self.MAX_CHUNK_SIZE) + ' bytes is' +
-                            ' not supported.  Your file is ' +
-                            str(datalen) + ' bytes.')
+                            str(self.MAX_CHUNK_SIZE) + ' bytes is not'
+                            ' supported. Your file is ' + str(datalen) +
+                            ' bytes.')
 
         filename = os.path.basename(filepath)
         params = {
@@ -204,14 +206,11 @@ class GirderClient(object):
 
         obj = self.sendRestRequest('POST', 'file', params)
 
-        uploadId = None
-
         if '_id' in obj:
             uploadId = obj['_id']
         else:
-            raise Exception('After creating an upload token, expected' +
-                            ' an object with an id.  Got instead: ' +
-                            json.dumps(obj))
+            raise Exception('After creating an upload token, expected an object'
+                            'with an id. Got instead: ' + json.dumps(obj))
 
         parameters = {
             'token': self.token,
@@ -223,25 +222,26 @@ class GirderClient(object):
             'chunk': data
         }
 
-        upResult = requests.post(self.urlBase + 'file/chunk', params=parameters, files=filedata)
+        upResult = requests.post(self.urlBase + 'file/chunk', params=parameters,
+                                 files=filedata)
         obj = upResult.json()
 
         if '_id' in obj:
             return obj['_id']
         else:
-            raise Exception('After uploading file as single chunk, did ' +
-                            'not receive object with _id.  Got instead: ' +
+            raise Exception('After uploading file as single chunk, did'
+                            ' not receive object with _id. Got instead: ' +
                             json.dumps(obj))
 
     #-------------------------------------------------------------------------
-    # A convenience method for getting an item by id.
+    # A convenience method for getting an item by id
     #-------------------------------------------------------------------------
-    def getItem(self, id):
+    def getItem(self, itemId):
         """
-        This function will retrieve an item by it's id
+        Retrieves an item by its ID.
 
-            id: A string containing the id of the item to retrieve from
+            itemId: A string containing the ID of the item to retrieve from
             Girder
         """
-        path = 'item/' + id
+        path = 'item/' + itemId
         return self.sendRestRequest('GET', path)
