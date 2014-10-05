0e85c40eee6476cea388bb8c07b0574afb70afd8
Jonathan Beezley <jonathan.beezley@kitware.com>
2014-10-05 17:41:43 -0400

    Adding a test for the front page version string

---
+++ clients/web/test/spec/versionSpec.js
@@ -0,0 +1,27 @@
+/**
+ * Start the girder backbone app.
+ */
+$(function () {
+    girder.events.trigger('g:appload.before');
+    var app = new girder.App({});
+    girder.events.trigger('g:appload.after');
+});
+
+describe('Test version reporting', function () {
+    it('check front page git SHA', function () {
+        waitsFor(function () {
+            return $('.g-frontpage-body').length > 0;
+        });
+        
+        runs(function () {
+            // Better checks are possible, but this at least ensures
+            // that the sha isn't 'undefined'.
+            expect($('.g-git-sha').length).toBe(1);
+            expect(!!$('.g-git-sha')
+                .text()
+                .toLowerCase()
+                .match(/^[0-9a-f]+$/)
+            ).toBe(true);
+        });
+    });
+});
