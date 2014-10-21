f3f1dce4d8839e00e501cca3561d5b2c731dd592
Zach Mullen <zach.mullen@kitware.com>
2014-10-20 21:05:28 -0400

    Fix aiur dashboard

--- cmake/run_aiur_dashboard.sh
+++ cmake/run_aiur_dashboard.sh
@@ -3,7 +3,7 @@
 source /usr/share/virtualenvwrapper/virtualenvwrapper.sh
 workon girder
 cd /home/cpatrick/Dashboards/girder
-pip install -r requirements.txt -r requirements-dev.txt -r plugins/metadata_extractor/requirements.txt
+pip install -r requirements.txt -r requirements-dev.txt -r plugins/metadata_extractor/requirements.txt -r plugins/geospatial/requirements.txt
 npm install
 python setup.py install
 /usr/local/bin/ctest -VV -S /home/cpatrick/Dashboards/girder/cmake/aiur_nightly.cmake
