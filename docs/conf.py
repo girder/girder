e2cefbaf4276b83b66c7d31b927e30ca57f1f55a
Zach Mullen <zach.mullen@kitware.com>
2014-10-27 11:04:44 -0400

    v1.0.1
    Bugfix release

--- docs/conf.py
+++ docs/conf.py
@@ -80,7 +80,7 @@ copyright = u'2014, Kitware'
 # built documents.
 #
 # The full version, including alpha/beta/rc tags.
-release = '1.0.0'
+release = '1.0.1'
 # The short X.Y version.
 version = '1.0'
 
