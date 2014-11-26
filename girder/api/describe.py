234b1734c00618eb3204992102e49026061a819a
Zach Mullen <zach.mullen@kitware.com>
2014-11-25 20:30:06 -0500

    Fixes #528. Dynamic swagger HTML page
    This allows us to choose the static root at runtime.

--- girder/api/describe.py
+++ girder/api/describe.py
@@ -18,6 +18,7 @@
 ###############################################################################
 
 import cherrypy
+import mako
 import os
 
 from girder.constants import ROOT_DIR, VERSION, PACKAGE_DIR
@@ -33,7 +34,7 @@ the top level package.json.
 """
 API_VERSION = VERSION['apiVersion']
 
-SWAGGER_VERSION = "1.2"
+SWAGGER_VERSION = '1.2'
 
 # Save the path to the swagger-ui on load
 _swagger_path = os.path.join(ROOT_DIR, 'clients')
@@ -146,13 +147,95 @@ class Description(object):
 
 
 class ApiDocs(object):
+    """
+    This serves up the swagger page.
+    """
     exposed = True
 
+    indexHtml = None
+
+    vars = {
+        'staticRoot': '',
+        'title': 'Girder - REST API Documentation'
+    }
+
+    template = r"""
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <title>${title}</title>
+        <link rel="stylesheet"
+            href="//fonts.googleapis.com/css?family=Droid+Sans:400,700">
+        <link rel="stylesheet"
+            href="${staticRoot}/lib/fontello/css/fontello.css">
+        <link rel="stylesheet"
+            href="${staticRoot}/built/swagger/css/reset.css">
+        <link rel="stylesheet"
+            href="${staticRoot}/built/swagger/css/screen.css">
+        <link rel="stylesheet"
+            href="${staticRoot}/built/swagger/docs.css">
+        <link rel="icon"
+            type="image/png"
+            href="${staticRoot}/img/Girder_Favicon.png">
+      </head>
+      <body>
+        <div class="docs-header">
+          <span>Girder REST API Documentation</span>
+          <i class="icon-book-alt right"></i>
+        </div>
+        <div class="docs-body">
+          <p>Below you will find the list of all of the resource types exposed
+          by the Girder RESTful Web API. Click any of the resource links to open
+          up a list of all available endpoints related to each resource type.
+          </p>
+          <p>Clicking any of those endpoints will display detailed documentation
+          about the purpose of each endpoint and the input parameters and output
+          values. You can also call API endpoints directly from this page by
+          typing in the parameters you wish to pass and then clicking the "Try
+          it out!" button.</p>
+          <p><b>Warning:</b> This is not a sandbox&mdash;calls that you make
+          from this page are the same as calling the API with any other client,
+          so update or delete calls that you make will affect the actual data on
+          the server.</p>
+        </div>
+        <div class="swagger-section">
+          <div id="swagger-ui-container"
+              class="swagger-ui-wrap docs-swagger-container">
+          </div>
+        </div>
+        <script src="${staticRoot}/built/swagger/lib/jquery-1.8.0.min.js">
+        </script>
+        <script src="${staticRoot}/built/swagger/lib/jquery.slideto.min.js">
+        </script>
+        <script src="${staticRoot}/built/swagger/lib/jquery.wiggle.min.js">
+        </script>
+        <script src="${staticRoot}/built/swagger/lib/jquery.ba-bbq.min.js">
+        </script>
+        <script src="${staticRoot}/built/swagger/lib/handlebars-1.0.0.js">
+        </script>
+        <script src="${staticRoot}/built/swagger/lib/underscore-min.js">
+        </script>
+        <script src="${staticRoot}/built/swagger/lib/backbone-min.js"></script>
+        <script src="${staticRoot}/built/swagger/lib/shred.bundle.js"></script>
+        <script src="${staticRoot}/built/swagger/lib/swagger.js"></script>
+        <script src="${staticRoot}/built/swagger/swagger-ui.min.js"></script>
+        <script src="${staticRoot}/built/swagger/lib/highlight.7.3.pack.js">
+        </script>
+        <script src="${staticRoot}/girder-swagger.js"></script>
+      </body>
+    </html>
+    """
+
+    def updateHtmlVars(self, vars):
+        self.vars.update(vars)
+        self.indexHtml = None
+
     def GET(self, **params):
-        return cherrypy.lib.static.serve_file(
-            _swagger_path,
-            content_type='text/html'
-        )
+        if self.indexHtml is None:
+            self.indexHtml = mako.template.Template(self.template).render(
+                **self.vars)
+
+        return self.indexHtml
 
     def DELETE(self, **params):
         raise cherrypy.HTTPError(405)
