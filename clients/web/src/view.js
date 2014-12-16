12e60b046dfa19adbdc07a15a7a70c44e155149e
Zach Mullen <zach.mullen@kitware.com>
2014-12-15 21:47:37 -0500

    Add utilities for allowing views to be GC'ed

--- clients/web/src/view.js
+++ clients/web/src/view.js
@@ -1,2 +1,22 @@
 girder.View = Backbone.View.extend({
+    /**
+     * Views should register their child girder.View objects so that they can
+     * be automatically destroyed when the parent is destroyed.
+     */
+    childViews: [],
+
+    /**
+     * Remove a view, unbinding its events and removing its listeners on
+     * girder.events so that it can be garbage collected.
+     */
+    destroy: function () {
+        this.$el.empty();
+        this.undelegateEvents();
+        this.off();
+        girder.events.off(null, null, this);
+
+        _.each(this.childViews, function (child) {
+            child.destroy();
+        });
+    }
 });
