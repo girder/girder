91467888c7fa488d408fa3240828f9577d6ac12b
Zach Mullen <zach.mullen@kitware.com>
2014-12-22 15:58:01 -0500

    Fix routing test
    This required reintroduction of our forcing the modal backdrop to
    be removed since the modal backdrop likes to stick around on
    phantomjs.

--- clients/web/test/testUtils.js
+++ clients/web/test/testUtils.js
@@ -422,12 +422,13 @@ girderTest.testMetadata = function () {
 girderTest.waitForLoad = function (desc) {
     desc = desc?' ('+desc+')':'';
     /*waitsFor(function() {
-        return !$('#g-dialog-container').html();
+        return $('#g-dialog-container:visible').length === 0;
     }, 'for the dialog container to be hidden'+desc);*/
     /* It is faster to wait to make sure a dialog is being hidden than to wait
      * for it to be fullt gone.  It is probably more reliable, too.  This had
      * been:*/
     waitsFor(function () {
+        $('.modal-backdrop').remove();
         return $('.modal-backdrop').length === 0;
     }, 'for the modal backdrop to go away');
 
