e15f529207884a25faa30967c7300672ae30cd32
Jonathan Beezley <jonathan.beezley@kitware.com>
2015-01-16 13:26:17 -0500

    Unset PYTHONPATH's before executing packaging tests
    When PYTHONPATH contains the repository directory, girder-install
    will detect the repository source as the install path.  Running
    the tests reinstall plugins and web source and remove some files
    that don't go in the distribution tarball.  This takes the
    heavy handed approach of removing the PYTHONPATH variable entirely,
    but for the purpose of these tests it probably doesn't matter much.

--- tests/packaging/pip_install_girder.sh
+++ tests/packaging/pip_install_girder.sh
@@ -2,6 +2,7 @@
 
 virtualenv_pip="${1}"
 PROJECT_SOURCE_DIR="${2}"
+unset PYTHONPATH
 
 "${virtualenv_pip}" uninstall girder > /dev/null
 "${virtualenv_pip}" install -U "${PROJECT_SOURCE_DIR}"/girder-[0-9].[0-9]*.tar.gz > /dev/null
