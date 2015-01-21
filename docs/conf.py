5413594216ff6c2ad81639489c6caf61109d2723
Jonathan Beezley <jonathan.beezley@kitware.com>
2015-01-21 13:50:22 -0500

    Creating release 1.2.1

--- docs/conf.py
+++ docs/conf.py
@@ -82,7 +82,7 @@ copyright = u'2014, Kitware'
 # built documents.
 #
 # The full version, including alpha/beta/rc tags.
-release = '1.2.0'
+release = '1.2.1'
 # The short X.Y version.
 version = '1.2'
 
