dc60f2c2e9658946c56906c607f3fb2f13945e4e
David Manthey <david.manthey@kitware.com>
2015-01-26 16:52:38 -0500

    Fix javascript coverage reporting in aiur.
    Whenever we add coverage of the javascript of a plugin, we needed to copy the templates.js file to a particular location.  I've attempted to automate and generalize this so that next time adding coverage won't break the js coverage report.

--- cmake/run_aiur_dashboard.sh
+++ cmake/run_aiur_dashboard.sh
@@ -6,5 +6,8 @@ cd /home/cpatrick/Dashboards/girder
 pip install -r requirements.txt -r requirements-dev.txt -r plugins/metadata_extractor/requirements.txt -r plugins/geospatial/requirements.txt -r plugins/celery_jobs/requirements.txt
 npm install
 python setup.py install
+# Copy compile plugin template files so that the javascript coverage locates
+# them properly.  There are certainly nicer ways to do this.
+find /home/cpatrick/Dashboards/girder/clients/web/static/built/plugins/ -name templates.js -exec python -c 'import shutil;path = """{}""";shutil.copy(path, path.replace("/clients/web/static/built/plugins/","/plugins/"))' \;
 /usr/local/bin/ctest -VV -S /home/cpatrick/Dashboards/girder/cmake/aiur_nightly.cmake
 /usr/local/bin/ctest -VV -S /home/cpatrick/Dashboards/girder/cmake/aiur_nightly_js_coverage.cmake
