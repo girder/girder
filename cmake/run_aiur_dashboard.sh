0505e2ec87723a49db7c159f84561335bc1cf3ed
David Manthey <david.manthey@kitware.com>
2015-01-29 08:22:14 -0500

    Increase jasmine timeout for automated builds.
    For automated builds (travis, dashboard), set the Jasmine timeout to 15 seconds.  I've noticed that when the build computer is loaded or has none of the necessary files in the file system cache, that builds succeed with larger timeouts.

--- cmake/run_aiur_dashboard.sh
+++ cmake/run_aiur_dashboard.sh
@@ -9,5 +9,5 @@ python setup.py install
 # Copy compile plugin template files so that the javascript coverage locates
 # them properly.  There are certainly nicer ways to do this.
 find /home/cpatrick/Dashboards/girder/clients/web/static/built/plugins/ -name templates.js -exec python -c 'import shutil;path = """{}""";shutil.copy(path, path.replace("/clients/web/static/built/plugins/","/plugins/"))' \;
-/usr/local/bin/ctest -VV -S /home/cpatrick/Dashboards/girder/cmake/aiur_nightly.cmake
+JASMINE_TIMEOUT=15000 /usr/local/bin/ctest -VV -S /home/cpatrick/Dashboards/girder/cmake/aiur_nightly.cmake
 /usr/local/bin/ctest -VV -S /home/cpatrick/Dashboards/girder/cmake/aiur_nightly_js_coverage.cmake
