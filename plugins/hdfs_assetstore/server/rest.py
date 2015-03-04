3cb0d9d99f658a8e1a11b899290d2f38ed12b9a4
Zach Mullen <zach.mullen@kitware.com>
2015-03-04 14:22:19 -0500

    Implement HDFS data import

--- plugins/hdfs_assetstore/server/rest.py
+++ plugins/hdfs_assetstore/server/rest.py
@@ -17,12 +17,15 @@
 #  limitations under the License.
 ###############################################################################
 
+import posixpath
+
 from girder.api import access
 from girder.api.describe import Description
-from girder.api.rest import Resource, loadmodel
+from girder.api.rest import Resource, loadmodel, RestException
 from girder.constants import AssetstoreType
 from girder.utility.progress import ProgressContext
 from snakebite.client import Client as HdfsClient
+from snakebite.errors import FileNotFoundException
 
 
 class HdfsAssetstoreResource(Resource):
@@ -31,16 +34,61 @@ class HdfsAssetstoreResource(Resource):
         self.route('PUT', (':id', 'import'), self.importData)
         self.route('POST', (), self.createAssetstore)
 
-    def _importData(self, parentType, parent, assetstore, client, path, ctx):
-        ctx.update(message='Importing ' + path)
+        self.folderModel = self.model('folder')  # Save to avoid many lookups
+        self.itemModel = self.model('item')
+        self.fileModel = self.model('file')
+
+    def _importFile(self, parent, name, user, assetstore, node):
+        item = self.itemModel.findOne({
+            'folderId': parent['_id'],
+            'name': name
+        })
+        if item is None:
+            item = self.itemModel.createItem(
+                name=name, creator=user, folder=parent)
+
+        file = self.fileModel.findOne({
+            'name': name,
+            'itemId': item['_id']
+        })
+        if file is None:
+            file = self.fileModel.createFile(
+                creator=user, item=item, name=name, size=node['length'],
+                assetstore=assetstore, mimeType=None, saveFile=False)
+
+        file['hdfs'] = {
+            'imported': True,
+            'path': node['path']
+        }
+        self.fileModel.save(file)
+
+    def _importData(self, parentType, parent, assetstore, client, path, ctx,
+                    user):
         for node in client.ls([path]):
-            print node  # TODO import logic
+            ctx.update(message='Importing ' + node['path'])
+            name = posixpath.basename(node['path'])
+
+            if node['file_type'] == 'd':
+                folder = self.folderModel.findOne({
+                    'parentId': parent['_id'],
+                    'name': name,
+                    'parentCollection': parentType
+                })
+                if folder is None:
+                    folder = self.folderModel.createFolder(
+                        parent, name, parentType=parentType, creator=user)
+
+                self._importData('folder', folder, assetstore, client,
+                                 node['path'], ctx, user)
+            elif node['file_type'] == 'f' and parentType == 'folder':
+                self._importFile(parent, name, user, assetstore, node)
 
     @access.admin
     @loadmodel(model='assetstore')
     def importData(self, assetstore, params):
         self.requireParams(('parentId', 'path'), params)
 
+        user = self.getCurrentUser()
         parentType = params.get('parentType', 'folder')
         if parentType not in ('user', 'collection', 'folder'):
             raise RestException('Invalid parentType.')
@@ -53,10 +101,15 @@ class HdfsAssetstoreResource(Resource):
         client = HdfsClient(
             host=assetstore['hdfs']['host'], port=assetstore['hdfs']['port'],
             use_trash=False)
-        with ProgressContext(progress, user=self.getCurrentUser(),
+        path = params['path']
+
+        with ProgressContext(progress, user=user,
                              title='Importing data from HDFS') as ctx:
-            self._importData(
-                parentType, parent, assetstore, client, params['path'], ctx)
+            try:
+                self._importData(
+                    parentType, parent, assetstore, client, path, ctx, user)
+            except FileNotFoundException:
+                raise RestException('File not found: %s.' % path)
     importData.description = (
         Description('Import a data hierarchy from an HDFS instance.')
         .notes('Only site administrators may use this endpoint.')
