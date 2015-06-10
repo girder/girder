731e295b8a4167f86e7870b2f0d97a189fc07ec1
Zach Mullen <zach.mullen@kitware.com>
2015-06-10 13:32:08 -0400

    Add top level view in web client for job details

--- plugins/jobs/web_client/js/views/JobDetailsWidget.js
+++ plugins/jobs/web_client/js/views/JobDetailsWidget.js
@@ -1,6 +1,10 @@
 girder.views.jobs_JobDetailsWidget = girder.View.extend({
     initialize: function (settings) {
         this.job = settings.job;
+
+        if (settings.renderImmediate) {
+            this.render();
+        }
     },
 
     render: function () {
@@ -12,4 +16,15 @@ girder.views.jobs_JobDetailsWidget = girder.View.extend({
 
         return this;
     }
+});
+
+girder.router.route('job/:id', 'jobView', function (id) {
+    var job = new girder.models.JobModel({_id: id}).once('g:fetched', function () {
+        girder.events.trigger('g:navigateTo', girder.views.jobs_JobDetailsWidget, {
+            job: job,
+            renderImmediate: true
+        });
+    }, this).once('g:error', function () {
+        girder.router.navigate('collections', {trigger: true});
+    }, this).fetch();
 });
