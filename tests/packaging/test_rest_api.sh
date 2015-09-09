ef2b8ff2a448b2ea3e887b3bfc25b526eb51baf2
Dan LaManna <dan.lamanna@kitware.com>
2015-09-09 17:57:07 -0400

    Revert "Debugging travis"
    This reverts commit e5e5df78c7b17751f80f69b3de5fb25991f718a8.

--- tests/packaging/test_rest_api.sh
+++ tests/packaging/test_rest_api.sh
@@ -8,14 +8,14 @@ unset PYTHONPATH
 source "${virtualenv_activate}"
 
 export GIRDER_PORT=31200
-python -m girder &
+python -m girder &> /dev/null &
 
 version="$(girder-install version)"
 echo "Detected api version ${version}"
 
 TIMEOUT=0
 until [ $TIMEOUT -eq 15 ]; do
-    json=$("${CURL}" http://localhost:${GIRDER_PORT}/api/v1/system/version)
+    json=$("${CURL}" -s http://localhost:${GIRDER_PORT}/api/v1/system/version)
     if [ -n "$json" ] && [[ $json == *shortSHA* ]]; then
         break
     fi
