b6ab0e0f481663a114d92f50c74d03f4cc03af9d
Zach Mullen <zach.mullen@kitware.com>
2015-09-29 14:11:58 -0400

    Various improvements
    * Add ProgressContext to our API docs
    * Fix hackish imports related to port detection in test env
    * Mock girder log write functions in test env

--- girder/utility/progress.py
+++ girder/utility/progress.py
@@ -31,19 +31,23 @@ class ProgressContext(ModelImporter):
     """
     This class is a context manager that can be used to update progress in a way
     that rate-limits writes to the database and guarantees a flush when the
-    context is exited.
-    """
-    def __init__(self, on, interval=0.5, **kwargs):
-        """
-        Create a new progress manager. This is a no-op if "on" is set to false,
-        which is convenient for the caller's semantics.
+    context is exited. This is a no-op if "on" is set to False, which is
+    meant as a convenience for callers. Any additional kwargs passed to this
+    constructor are passed through to the ``initProgress`` method of the
+    notification model.
 
-        :param on: Whether to record progress.
-        :type on: bool
-        :param interval: Minimum time interval at which to write updates
-        to the database, in seconds.
-        :type interval: int or float
-        """
+    :param on: Whether to record progress.
+    :type on: bool
+    :param interval: Minimum time interval at which to write updates to the
+        database, in seconds.
+    :type interval: int or float
+    :param user: The user creating this progress.
+    :type user: dict
+    :param title: The title for the task being tracked.
+    :type title: str
+    """
+
+    def __init__(self, on, interval=0.5, **kwargs):
         self.on = on
         self.interval = interval
 
@@ -84,8 +88,8 @@ class ProgressContext(ModelImporter):
         the last time the record was written to the database. Accepts the
         same kwargs as Notification.updateProgress.
 
-        :param force: Whether we should force the write to the database.
-        Use only in cases where progress may be indeterminate for a long time.
+        :param force: Whether we should force the write to the database. Use
+            only in cases where progress may be indeterminate for a long time.
         :type force: bool
         """
         # Extend the response timeout, even if we aren't reporting the progress
@@ -110,8 +114,8 @@ def setResponseTimeLimit(duration=600, onlyExtend=True):
     which can take longer should call this function.
 
     Note that for cherrypy responses that include streaming generator
-    functions,such as downloads, the timeout is only relevant until the first
-    `yield` is reached.  As such, long running generator responses do not
+    functions, such as downloads, the timeout is only relevant until the first
+    ``yield`` is reached.  As such, long running generator responses do not
     generally need to call this function.
 
     :param duration: additional duration in seconds to allow for the response.
