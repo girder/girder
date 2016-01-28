7131d796571205643d38f3bcb26944774230afbc
Max Smolens <max.smolens@kitware.com>
2016-01-28 14:38:12 -0500

    Upgrade to swagger-ui 2.1.4

--- clients/web/static/girder-swagger.js
+++ clients/web/static/girder-swagger.js
@@ -8,11 +8,13 @@ $(function () {
         url: 'describe',
         dom_id: 'swagger-ui-container',
         supportHeaderParams: false,
-        supportedSubmitMethods: ['get', 'post', 'put', 'delete'],
+        supportedSubmitMethods: ['get', 'post', 'put', 'delete', 'patch'],
         onComplete: function (swaggerApi, swaggerUi) {
             $('pre code').each(function (i, e) {
                 hljs.highlightBlock(e);
             });
+
+            addApiKeyAuthorization();
         },
         onFailure: function (data) {
             if (console) {
@@ -20,20 +22,29 @@ $(function () {
                 console.log(data);
             }
         },
-        docExpansion: "none"
+        docExpansion: "none",
+        jsonEditor: false,
+        apisSorter: "alpha",
+        defaultModelRendering: "schema",
+        showRequestHeaders: false,
+        validatorUrl: null
     });
 
-    var cookieParams = document.cookie.split(';').map(function (m) {
-        return m.replace(/^\s+/, '').replace(/\s+$/, '');
-    });
-    $.each(cookieParams, function (i, val) {
-        var arr = val.split('=');
-        if (arr[0] === 'girderToken') {
-            // Make swagger send the Girder-Token header with each request.
-            window.authorizations.add("key",
-                new ApiKeyAuthorization("Girder-Token", arr[1], "header"));
-        }
-    });
+    function addApiKeyAuthorization() {
+        var cookieParams = document.cookie.split(';').map(function (m) {
+            return m.replace(/^\s+/, '').replace(/\s+$/, '');
+        });
+        $.each(cookieParams, function (i, val) {
+            var arr = val.split('=');
+            if (arr[0] === 'girderToken') {
+                // Make swagger send the Girder-Token header with each request.
+                var apiKeyAuth = new SwaggerClient.ApiKeyAuthorization(
+                    "Girder-Token", arr[1], "header");
+                window.swaggerUi.api.clientAuthorizations.add(
+                    "Girder-Token", apiKeyAuth);
+            }
+        });
+    }
 
-    swaggerUi.load();
+    window.swaggerUi.load();
 });
