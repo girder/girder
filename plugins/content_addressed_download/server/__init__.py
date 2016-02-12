88d59679471b8f13ca3af174197d2cf844e02026
Johan Andruejol <johan.andruejol@kitware.com>
2016-02-12 17:15:22 -0500

    Very basic api route to be able to download a file using sha512

---
+++ plugins/content_addressed_download/server/__init__.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+###############################################################################
+#  Copyright Kitware Inc.
+#
+#  Licensed under the Apache License, Version 2.0 ( the "License" );
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+###############################################################################
+
+from girder.api import access
+from girder.api.describe import describeRoute, Description
+from girder.api.v1.file import File
+
+class HashedFile(File):
+
+    def __init__(self, apiRoot):
+        super(File, self).__init__()
+
+        self.resourceName = 'file'
+        apiRoot.file.route('GET', ('content', ':algo', ':hash', 'download'),
+                           self.downloadWithHash)
+
+    @access.public
+    @describeRoute(
+        Description('Download a file by its hash.')
+        .param('algo', 'The type of the given hash.',
+               paramType='path', enum=['sha521'])
+        .param('hash', 'The hash of the file to download.',
+                paramType='path')
+        .errorResponse()
+        .errorResponse('Read access was denied on the file.', 403)
+    )
+    def downloadWithHash(self, algo, hash, params):
+        query = { algo : hash }
+        fileModel = self.model('file')
+        file = fileModel.findOne(query)
+        return fileModel.download(file)
+
+def load(info):
+    HashedFile(info['apiRoot'])
