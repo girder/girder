99fbfcd8bdc2dd6d560f3b1635ac9939ce98c1e0
Harold Rodriguez <harold@planetharold.com>
2016-03-25 11:24:52 -0400

    add eslint test

--- plugins/item_previews/plugin.cmake
+++ plugins/item_previews/plugin.cmake
@@ -1,1 +1,2 @@
 add_web_client_test(item_previews "${PROJECT_SOURCE_DIR}/plugins/item_previews/plugin_tests/item_previews.js" PLUGIN item_previews)
+add_eslint_test(item_previews "${PROJECT_SOURCE_DIR}/plugins/item_previews/web_client/js")
