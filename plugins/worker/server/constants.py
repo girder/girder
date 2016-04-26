e8bb696711d87359d1b6c3042e3fc4d6a9440f2d
Zach Mullen <zach.mullen@kitware.com>
2016-04-26 10:11:41 -0400

    Add constants for mounted dirs in docker tasks
    ping @cdeepakroy

---
+++ plugins/worker/server/constants.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+###############################################################################
+#  Copyright Kitware Inc.
+#
+#  Licensed under the Apache License, Version 2.0 ( the "License" );
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+###############################################################################
+
+# The path that will be mounted in docker containers for data IO
+DOCKER_DATA_VOLUME = '/mnt/girder_worker/data'
+
+# The path that will be mounted in docker containers for utility scripts
+DOCKER_SCRIPTS_VOUME = '/mnt/girder_worker/scripts'
