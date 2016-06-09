158fe39b4e3fba22d75fde56559a024212c70c71
Michael Fogleman <fogleman@gmail.com>
2016-06-09 14:35:02 -0400

    2016 Copyright

--- plugins/homepage/server/__init__.py
+++ plugins/homepage/server/__init__.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 ###############################################################################
-#  Copyright Kitware Inc.
+#  Copyright 2016 Kitware Inc.
 #
 #  Licensed under the Apache License, Version 2.0 ( the "License" );
 #  you may not use this file except in compliance with the License.
