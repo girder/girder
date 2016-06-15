c5b65c6c733afdb33dc07ccc3f79614896417920
Michael Fogleman <fogleman@gmail.com>
2016-06-15 14:11:02 -0400

    Add web client tests for curation plugin

--- plugins/curation/plugin.cmake
+++ plugins/curation/plugin.cmake
@@ -3,8 +3,8 @@ add_python_test(curation PLUGIN curation)
 add_python_style_test(python_static_analysis_curation
     "${PROJECT_SOURCE_DIR}/plugins/curation/server")
 
-# add_python_style_test(python_static_analysis_curation_tests
-#     "${PROJECT_SOURCE_DIR}/plugins/curation/plugin_tests")
+add_python_style_test(python_static_analysis_curation_tests
+    "${PROJECT_SOURCE_DIR}/plugins/curation/plugin_tests")
 
 add_eslint_test(curation
     "${PROJECT_SOURCE_DIR}/plugins/curation/web_client/js")
--- plugins/curation/plugin_tests/curationSpec.js
+++ plugins/curation/plugin_tests/curationSpec.js
@@ -9,8 +9,109 @@ girderTest.importStylesheet(
 
 girderTest.startApp();
 
-describe('Test the curation UI.', function () {
-    it('register a user', girderTest.createUser(
-        'johndoe', 'john.doe@email.com', 'John', 'Doe', 'password!'
+function _goToCurationDialog() {
+    girderTest.waitForLoad();
+    waitsFor(function () {
+        return $('button.g-folder-actions-button:visible').length === 1;
+    }, 'folder actions button to be visible');
+    runs(function () {
+        $('button.g-folder-actions-button:visible').click();
+    });
+    waitsFor(function () {
+        return $('a.g-curation-button:visible').length === 1;
+    }, 'curation button to be visible');
+    runs(function () {
+        $('a.g-curation-button:visible').click();
+    });
+    waitsFor(function () {
+        return $('.g-curation-summary:visible').length === 1;
+    }, 'the curation dialog to appear');
+}
+
+describe('test the curation ui', function () {
+    it('register an admin', girderTest.createUser(
+        'admin', 'admin@example.com', 'Joe', 'Admin', 'password'
     ));
+
+    waitsFor(function () {
+        return $('a.g-my-folders').length > 0;
+    }, 'my folders link to load');
+    runs(function () {
+        $('a.g-my-folders').click();
+    });
+
+    waitsFor(function () {
+        return $('a.g-folder-list-link:contains(Public)').length > 0;
+    }, 'the folders list to load');
+    runs(function () {
+        $('a.g-folder-list-link:contains(Public)').click();
+    });
+
+    _goToCurationDialog();
+
+    runs(function() {
+        expect($('.g-curation-summary:visible').text()).toContain('disabled');
+        expect($('#g-curation-enable:visible').length).toBe(1);
+        expect($('#g-curation-request:visible').length).toBe(0);
+        expect($('#g-curation-approve:visible').length).toBe(0);
+        expect($('#g-curation-reject:visible').length).toBe(0);
+        expect($('#g-curation-reopen:visible').length).toBe(0);
+        expect($('#g-curation-disable:visible').length).toBe(0);
+    });
+    runs(function () {
+        $('#g-curation-enable:visible').click();
+    });
+    waitsFor(function () {
+        return $('.g-curation-summary:visible').length === 0;
+    }, 'the curation dialog to disappear');
+
+    _goToCurationDialog();
+    runs(function() {
+        expect($('.g-curation-summary:visible').text()).toContain('enabled');
+        expect($('.g-curation-summary:visible').text()).toContain('construction');
+        expect($('#g-curation-enable:visible').length).toBe(0);
+        expect($('#g-curation-request:visible').length).toBe(1);
+        expect($('#g-curation-approve:visible').length).toBe(0);
+        expect($('#g-curation-reject:visible').length).toBe(0);
+        expect($('#g-curation-reopen:visible').length).toBe(0);
+        expect($('#g-curation-disable:visible').length).toBe(1);
+    });
+
+    runs(function () {
+        $('#g-curation-request:visible').click();
+    });
+    waitsFor(function () {
+        return $('.g-curation-summary:visible').length === 0;
+    }, 'the curation dialog to disappear');
+
+    _goToCurationDialog();
+    runs(function() {
+        expect($('.g-curation-summary:visible').text()).toContain('enabled');
+        expect($('.g-curation-summary:visible').text()).toContain('requested');
+        expect($('#g-curation-enable:visible').length).toBe(0);
+        expect($('#g-curation-request:visible').length).toBe(0);
+        expect($('#g-curation-approve:visible').length).toBe(1);
+        expect($('#g-curation-reject:visible').length).toBe(1);
+        expect($('#g-curation-reopen:visible').length).toBe(0);
+        expect($('#g-curation-disable:visible').length).toBe(1);
+    });
+
+    runs(function () {
+        $('#g-curation-approve:visible').click();
+    });
+    waitsFor(function () {
+        return $('.g-curation-summary:visible').length === 0;
+    }, 'the curation dialog to disappear');
+
+    _goToCurationDialog();
+    runs(function() {
+        expect($('.g-curation-summary:visible').text()).toContain('enabled');
+        expect($('.g-curation-summary:visible').text()).toContain('approved');
+        expect($('#g-curation-enable:visible').length).toBe(0);
+        expect($('#g-curation-request:visible').length).toBe(0);
+        expect($('#g-curation-approve:visible').length).toBe(0);
+        expect($('#g-curation-reject:visible').length).toBe(0);
+        expect($('#g-curation-reopen:visible').length).toBe(1);
+        expect($('#g-curation-disable:visible').length).toBe(1);
+    });
 });
