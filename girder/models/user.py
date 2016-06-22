1ad5d10efbe266ac487cd5e453b2d19644192e90
Michael Fogleman <fogleman@gmail.com>
2016-06-22 11:15:29 -0400

    Handle no auto-login in FE. Admins don't need approval.

--- girder/models/user.py
+++ girder/models/user.py
@@ -268,6 +268,8 @@ class User(AccessControlledModel):
         Returns True if email verification is required and this user has not
         yet verified their email address.
         """
+        if user.get('admin'):
+            return False
         if not user.get('emailVerified', False):
             return self.model('setting').get(
                 SettingKey.EMAIL_VERIFICATION) == 'required'
@@ -278,6 +280,8 @@ class User(AccessControlledModel):
         Returns True if the registration policy requires admin approval and
         this user has not yet been approved.
         """
+        if user.get('admin'):
+            return False
         if user.get('status') != 'enabled':
             return self.model('setting').get(
                 SettingKey.REGISTRATION_POLICY) == 'approve'
