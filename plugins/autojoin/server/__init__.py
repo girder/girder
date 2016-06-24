2e3ff807635105ce1c04a119a2ebc01a46081dfd
Michael Fogleman <fogleman@gmail.com>
2016-06-24 15:07:21 -0400

    Perform autojoin on model.user.save.created event

--- plugins/autojoin/server/__init__.py
+++ plugins/autojoin/server/__init__.py
@@ -1,12 +1,22 @@
 from girder import events
-
-KEY = 'autojoin'
-
+from girder.utility.model_importer import ModelImporter
 
 def validateSettings(event):
-    if event.info['key'] == KEY:
+    if event.info['key'] == 'autojoin':
         event.preventDefault().stopPropagation()
+
+
+def userCreated(event):
+    user = event.info
+    email = user.get('email').lower()
+    rules = ModelImporter.model('setting').get('autojoin', [])
+    for rule in rules:
+        if rule['pattern'].lower() not in email:
+            continue
+        group = ModelImporter.model('group').load(rule['groupId'], force=True)
+        ModelImporter.model('group').addUser(group, user, rule['level'])
 
 
 def load(info):
     events.bind('model.setting.validate', 'autojoin', validateSettings)
+    events.bind('model.user.save.created', 'autojoin', userCreated)
