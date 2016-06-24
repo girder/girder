a96932c8f619629196b117bb4723936ce681f68b
Michael Fogleman <fogleman@gmail.com>
2016-06-24 15:17:32 -0400

    Add some docstrings

--- plugins/autojoin/server/__init__.py
+++ plugins/autojoin/server/__init__.py
@@ -2,11 +2,18 @@ from girder import events
 from girder.utility.model_importer import ModelImporter
 
 def validateSettings(event):
+    """
+    Allow the autojoin setting key.
+    """
     if event.info['key'] == 'autojoin':
         event.preventDefault().stopPropagation()
 
 
 def userCreated(event):
+    """
+    Check auto join rules when a new user is created. If a match is found,
+    add the user to the group with the specified access level.
+    """
     user = event.info
     email = user.get('email').lower()
     rules = ModelImporter.model('setting').get('autojoin', [])
