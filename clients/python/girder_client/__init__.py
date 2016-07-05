6650268e4bd375265a2467d6b8f9908e7dbc3d9a
Zach Mullen <zach.mullen@kitware.com>
2016-07-05 11:14:45 -0400

    Move `transformFilename` into the public API
    This is generally useful for someone who wants to hook into
    lower-level functionality of the client.

--- clients/python/girder_client/__init__.py
+++ clients/python/girder_client/__init__.py
@@ -818,9 +818,13 @@ class GirderClient(object):
         obj = self.put(path, json=metadata)
         return obj
 
-    def _transformFilename(self, name):
+    def transformFilename(self, name):
         """
-        Sanitize the filename a bit.
+        Sanitize a resource name from Girder into a name that is safe to use
+        as a filesystem path.
+
+        :param name: The name to transform.
+        :type name: str
         """
         if name in ('.', '..'):
             name = '_' + name
@@ -878,16 +882,16 @@ class GirderClient(object):
                 if len(files) == 1 and files[0]['name'] == name:
                     self.downloadFile(
                         files[0]['_id'],
-                        os.path.join(dest, self._transformFilename(name)))
+                        os.path.join(dest, self.transformFilename(name)))
                     break
                 else:
-                    dest = os.path.join(dest, self._transformFilename(name))
+                    dest = os.path.join(dest, self.transformFilename(name))
                     _safeMakedirs(dest)
 
             for file in files:
                 self.downloadFile(
                     file['_id'],
-                    os.path.join(dest, self._transformFilename(file['name'])))
+                    os.path.join(dest, self.transformFilename(file['name'])))
 
             first = False
             offset += len(files)
@@ -913,7 +917,7 @@ class GirderClient(object):
 
             for folder in folders:
                 local = os.path.join(
-                    dest, self._transformFilename(folder['name']))
+                    dest, self.transformFilename(folder['name']))
                 _safeMakedirs(local)
 
                 self.downloadFolderRecursive(folder['_id'], local)
