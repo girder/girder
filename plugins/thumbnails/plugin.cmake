daf32ea95928343a34f85e41ae18f11f93b2b2ef
Stuart Nelson <stuart.blake.nelson@gmail.com>
2016-08-04 16:26:57 -0600

    dicom test moved to python 2.7 testing only

--- plugins/thumbnails/plugin.cmake
+++ plugins/thumbnails/plugin.cmake
@@ -1,4 +1,5 @@
 add_python_test(thumbnail PLUGIN thumbnails)
+add_python_test(thumbnail_py2 PLUGIN thumbnails PY2_ONLY)
 add_python_style_test(python_static_analysis_thumbnails
                       "${PROJECT_SOURCE_DIR}/plugins/thumbnails/server")
 add_python_style_test(python_static_analysis_thumbnails_tests
