ce712db90a410acde26ed4515c25fa27526aff5f
Michael Fogleman <fogleman@gmail.com>
2016-08-10 12:21:41 -0400

    fix for py3

--- plugins/dicom_viewer/plugin.cmake
+++ plugins/dicom_viewer/plugin.cmake
@@ -3,8 +3,8 @@ add_python_test(dicom_viewer PLUGIN dicom_viewer)
 add_python_style_test(python_static_analysis_dicom_viewer
     "${PROJECT_SOURCE_DIR}/plugins/dicom_viewer/server")
 
-# add_python_style_test(python_static_analysis_dicom_viewer_tests
-#     "${PROJECT_SOURCE_DIR}/plugins/dicom_viewer/plugin_tests")
+add_python_style_test(python_static_analysis_dicom_viewer_tests
+    "${PROJECT_SOURCE_DIR}/plugins/dicom_viewer/plugin_tests")
 
 add_eslint_test(dicom_viewer
     "${PROJECT_SOURCE_DIR}/plugins/dicom_viewer/web_external/js")
