464f2d22826ec69f1a176043ec82db1596b2da87
Zach Mullen <zach.mullen@kitware.com>
2016-10-20 22:22:16 -0400

    Add web client test for authorized upload plugin

--- plugins/authorized_upload/plugin.cmake
+++ plugins/authorized_upload/plugin.cmake
@@ -2,6 +2,11 @@ get_filename_component(PLUGIN ${CMAKE_CURRENT_LIST_DIR} NAME)
 
 add_python_test(upload PLUGIN ${PLUGIN})
 
+add_web_client_test(
+    ${PLUGIN}
+    "${PROJECT_SOURCE_DIR}/plugins/${PLUGIN}/plugin_tests/authorizedUploadSpec.js"
+    PLUGIN ${PLUGIN})
+
 add_python_style_test(python_static_analysis_${PLUGIN}
     "${PROJECT_SOURCE_DIR}/plugins/${PLUGIN}/server")
 add_python_style_test(python_static_analysis_${PLUGIN}_tests
