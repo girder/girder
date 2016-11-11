19237dd6daa79ad0988260724b0c722959e2af16
Jonathan Beezley <jonathan.beezley@kitware.com>
2016-11-11 15:19:11 -0500

    Add cmake script for circle ci

---
+++ cmake/circle_continuous.cmake
@@ -0,0 +1,43 @@
+set(CTEST_SOURCE_DIRECTORY "$ENV{HOME}/girder")
+set(CTEST_BINARY_DIRECTORY "$ENV{HOME}/_build")
+
+include(${CTEST_SOURCE_DIRECTORY}/CTestConfig.cmake)
+if($ENV{CIRCLE_NODE_INDEX} EQUAL "0")
+  set(test_group "python")
+elseif($ENV{CIRCLE_NODE_INDEX} EQUAL "1")
+  set(test_group "browser")
+endif()
+
+set(CTEST_SITE "CircleCI")
+set(CTEST_BUILD_NAME "Linux-$ENV{CIRCLE_BRANCH}-Mongo-$ENV{MONGO_VERSION}-${test_group}")
+set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
+
+ctest_start("Continuous")
+ctest_configure(
+  OPTIONS "-DPYTHON_COVERAGE=ON"
+)
+ctest_build()
+
+if(test_group STREQUAL python)
+  ctest_test(
+    PARALLEL_LEVEL 4 RETURN_VALUE res
+    INCLUDE_LABEL girder_python
+  )
+elseif(test_group STREQUAL browser)
+  ctest_test(
+    PARALLEL_LEVEL 2 RETURN_VALUE res
+    EXCLUDE_LABEL girder_python
+  )
+  file(RENAME "${CTEST_BINARY_DIRECTORY}/coverage/js_coverage.xml" "${CTEST_BINARY_DIRECTORY}/coverage.xml")
+endif()
+
+ctest_coverage()
+
+file(REMOVE "${CTEST_BINARY_DIRECTORY}/coverage.xml")
+ctest_submit()
+
+file(REMOVE "${CTEST_BINARY_DIRECTORY}/test_failed")
+if(NOT res EQUAL 0)
+  file(WRITE "${CTEST_BINARY_DIRECTORY}/test_failed" "error")
+  message(FATAL_ERROR "Test failures occurred.")
+endif()
