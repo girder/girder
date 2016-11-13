8a771abda64fb0b485f0f9e42f3c2637e4b6b234
Jonathan Beezley <jonathan.beezley@kitware.com>
2016-11-12 20:15:37 -0500

    Submit coverage from all containers

--- cmake/run_circleci.sh
+++ cmake/run_circleci.sh
@@ -18,8 +18,14 @@ case $CIRCLE_NODE_INDEX in
 		exit 1
 esac
 
+mkdir $HOME/build
 touch $HOME/build/test_failed
 ctest -VV -S $HOME/girder/cmake/circle_continuous.cmake
 if [ -f $HOME/build/test_failed ] ; then
 	exit 1
 fi
+
+mkdir -p $CIRCLE_ARTIFACTS/coverage/python $CIRCLE_ARTIFACTS/coverage/js
+cp -r ~/build/coverage.xml ~/girder/clients/web/dev/built/py_coverage/* $CIRCLE_ARTIFACTS/coverage/python
+cp -r ~/build/coverage/* $CIRCLE_ARTIFACTS/coverage/js
+bash <(curl -s https://codecov.io/bash) || echo "Codecov did not collect coverage reports"
