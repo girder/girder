abb350f642720488241d03f399a63fee692b19a6
Zach Mullen <zach.mullen@kitware.com>
2016-12-08 15:50:28 -0500

    IO validation and front-end for executing task

--- plugins/worker_tasks/web_client/views/TaskRunView.js
+++ plugins/worker_tasks/web_client/views/TaskRunView.js
@@ -2,12 +2,17 @@ import WidgetModel from 'girder_plugins/slicer_cli_web/models/WidgetModel';
 import WidgetCollection from 'girder_plugins/slicer_cli_web/collections/WidgetCollection';
 import ControlsPanel from 'girder_plugins/slicer_cli_web/views/ControlsPanel';
 import View from 'girder/views/View';
+import { restRequest } from 'girder/rest';
 import { renderMarkdown } from 'girder/misc';
 
 import template from '../templates/taskRun.pug';
 import '../stylesheets/taskRun.styl';
 
 const TaskRunView = View.extend({
+    events: {
+        'click .g-run-task': 'execute'
+    },
+
     initialize: function () {
         this._taskSpec = this.model.get('meta').workerTaskSpec || {};
         this._inputs = this._taskSpec.inputs || [];
@@ -19,14 +24,16 @@ const TaskRunView = View.extend({
         this._inputWidgets.add(this._inputs.map((input) => {
             return new WidgetModel({
                 type: input.type,
-                title: input.name || input.id
-
+                title: input.name || input.id,
+                id: input.id || input.name
             });
         }));
+
         this._outputWidgets.add(this._outputs.map((output) => {
             return new WidgetModel({
                 type: output.type,
-                title: output.name || output.id
+                title: output.name || output.id,
+                id: output.id || output.name
             });
         }));
 
@@ -56,6 +63,97 @@ const TaskRunView = View.extend({
         if (this._outputs.length) {
             this._outputsPanel.setElement(this.$('.g-outputs-container')).render();
         }
+    },
+
+    /**
+     * Validates that all of the widgets are in a valid state. Displays any
+     * invalid states and
+     */
+    validate: function () {
+        let ok = true;
+        const test = (model) => {
+            if(!model.isValid()) {
+               ok = false;
+            }
+        };
+
+        // Don't short-circuit; we want to highlight *all* invalid inputs
+        this._inputWidgets.each(test);
+        this._outputWidgets.each(test);
+
+        return ok;
+    },
+
+    /**
+     * Translates the WidgetCollection state for the input and output widgets into the
+     * appropriate girder_worker input and output bindings, then invokes the endpoint
+     * to execute the task. TODO We probably want to move that translation layer down into
+     * the WidgetCollection itself in the future.
+     */
+    execute: function (e) {
+        if (!this.validate()) {
+            this.$('.g-validation-failed-message').text(
+                'One or more of your inputs or outputs is invalid, they are highlighted in red.');
+            return;
+        }
+        this.$('.g-validation-failed-message').empty();
+        $(e.currentTarget).attr('disabled', 'true').addClass('disabled');
+
+        const inputs = {},
+              outputs = {};
+
+        const translate = (model) => {
+            const val = model.value();
+
+            switch (model.get('type')) {
+                case 'image': // This is an input
+                    return {
+                        mode: 'girder',
+                        resource_type: 'file',
+                        id: val.id,
+                    };
+                case 'file': // This is an input
+                    return {
+                        mode: 'girder',
+                        resource_type: 'item',
+                        id: val.id,
+                    };
+                case 'new-file': // This is an output
+                    return {
+                        mode: 'girder',
+                        parent_id: model.value().get('folderId'),
+                        name: val.name()
+                    };
+                default:
+                    return {
+                        mode: 'inline',
+                        data: JSON.stringify(model.value())
+                    };
+            }
+        };
+
+        this._inputWidgets.each((model) => {
+            inputs[model.id] = translate(model);
+        });
+        this._outputWidgets.each((model) => {
+            outputs[model.id] = translate(model);
+        });
+
+        restRequest({
+            path: `worker_task/${this.model.id}/execution`,
+            type: 'POST',
+            data: {
+                inputs: JSON.stringify(inputs),
+                outputs: JSON.stringify(outputs)
+            },
+            error: null
+        }).done((resp) => {
+            console.log(resp);
+            router.navigate(`job/${resp.id}`);
+        }).error((resp) => {
+            $(e.currentTarget).attr('disabled', 'true').addClass('disabled');
+            this.$('.g-validation-failed-message').text('Error: ' + resp.responseJSON.message);
+        });
     }
 });
 
