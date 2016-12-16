eb6ddcf576265ed977be55beba25ea2b5aa2f9ed
Zach Mullen <zach.mullen@kitware.com>
2016-12-16 14:56:52 -0500

    Use require.resolve for more robust path lookup

--- scripts/postNpmInstall.js
+++ scripts/postNpmInstall.js
@@ -1,10 +1,13 @@
 // Remove the webpack-dev-server grunt task since we do not use it
+
 var fs = require('fs');
-var path = require('path');
+var taskFile = null;
 
-var taskFile = path.join(__dirname, '..', 'node_modules', 'grunt-webpack', 'tasks', 'webpack-dev-server.js');
+try {
+    taskFile = require.resolve('grunt-webpack/tasks/webpack-dev-server.js');
+} catch(e) {}
 
-if (fs.existsSync(taskFile)) {
+if (taskFile) {
     console.log('Removing unused task file ' + taskFile);
     fs.unlinkSync(taskFile);
 }
