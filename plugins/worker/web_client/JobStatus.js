04d6b1675a139d0d20eba9fcdbedfae6f21190d6
Zach Mullen <zach.mullen@kitware.com>
2016-12-20 11:29:08 -0500

    Add/cleanup comments

--- plugins/worker/web_client/JobStatus.js
+++ plugins/worker/web_client/JobStatus.js
@@ -1,3 +1,6 @@
+// Since plugins are not dynamically linked against other plugin libraries,
+// we have to modify the runtime global JobStatus, rather than importing it
+// here statically, which would only modify a local copy.
 girder.plugins.jobs.JobStatus.registerStatus({
     WORKER_FETCHING_INPUT: {
         value: 820,
