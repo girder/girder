0b8c7f3e5b79eb6b062dba1cd13065022a36310b
Zach Mullen <zach.mullen@kitware.com>
2016-12-20 10:52:50 -0500

    Fix girder-client python doc build on RTD
    This had broken silently because the setup.py install of
    girder_client is not run, and the build process tried to
    import diskcache via girder_client.

--- docs/conf.py
+++ docs/conf.py
@@ -47,7 +47,7 @@ class Mock(object):
         else:
             return Mock()
 
-MOCK_MODULES = ['bcrypt']
+MOCK_MODULES = ['bcrypt', 'diskcache']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = Mock()
 
