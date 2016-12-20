8b1c9b8d0659d53ac68d8aea7d1e962050bba806
Zach Mullen <zach.mullen@kitware.com>
2016-12-20 14:17:18 -0500

    Must declare global girder for eslint

--- plugins/worker/web_client/JobStatus.js
+++ plugins/worker/web_client/JobStatus.js
@@ -1,6 +1,8 @@
 // Since plugins are not dynamically linked against other plugin libraries,
 // we have to modify the runtime global JobStatus, rather than importing it
 // here statically, which would only modify a local copy.
+
+/*global girder*/
 girder.plugins.jobs.JobStatus.registerStatus({
     WORKER_FETCHING_INPUT: {
         value: 820,
