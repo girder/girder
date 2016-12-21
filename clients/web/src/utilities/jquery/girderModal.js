731a3af6428ce9a008975d7033f4296aa337b236
Zach Mullen <zach.mullen@kitware.com>
2016-12-21 14:07:26 -0500

    Attempt to fix race condition in web_client_routing

--- clients/web/src/utilities/jquery/girderModal.js
+++ clients/web/src/utilities/jquery/girderModal.js
@@ -11,23 +11,12 @@ import 'bootstrap/js/modal';
  *             to just close an open modal dialog.
  */
 $.fn.girderModal = function (view) {
-    var that = this;
     /* If we have a modal dialog open, or one is in the process of closing,
      * close that dialog before opening the new one.  This prevents
      * layering modal dialogs, but also makes sure that we don't have a
      * problem switching from one modal dialog to another. */
     if ($(this).is('.modal')) {
-        /* We have to reach into the backbone modal object a little to see
-         * if we need to do anything.  By turning off the fade as we
-         * remove the old dialog, the removal is synchronous and we are
-         * sure it is gone before we add the new dialog. */
-        if ($(this).data('bs.modal') && $(this).data('bs.modal').isShown) {
-            var elem = $($(this).data('bs.modal').$element);
-            var hasFade = elem.hasClass('fade');
-            elem.removeClass('fade');
-            $(this).modal('hide');
-            elem.toggleClass('fade', hasFade);
-        }
+        $(this).modal('hide');
     }
     if (view !== 'close') {
         this.off();
@@ -36,9 +25,9 @@ $.fn.girderModal = function (view) {
         // the modal (and sending the 'shown.bs.modal' event) *before* we get to
         // register the event in .on('shown.bs.modal', cb). Let's show
         // the modal in the next animation frame to fix this behavior for now.
-        setTimeout(function () {
-            that.modal().find('[data-dismiss="modal"]').unbind('click').click(function () {
-                that.modal('hide');
+        setTimeout(() => {
+            this.modal().find('[data-dismiss="modal"]').unbind('click').click(() => {
+                this.modal('hide');
             });
         }, 0);
         if (view !== false) {
