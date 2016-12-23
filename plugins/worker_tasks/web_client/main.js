051abe53a51e17a1267d5a43b501ce890d872d89
Zach Mullen <zach.mullen@kitware.com>
2016-12-22 21:43:02 -0500

    Add provenance info to job details view for worker tasks

--- plugins/worker_tasks/web_client/main.js
+++ plugins/worker_tasks/web_client/main.js
@@ -1,3 +1,4 @@
+import $ from 'jquery';
 import './routes';
 
 import { wrap } from 'girder/utilities/PluginUtils';
@@ -12,4 +13,20 @@ wrap(GlobalNavView, 'initialize', function (initialize) {
         icon: 'icon-cog-alt',
         target: 'worker_tasks'
     });
+});
+
+// Show task inputs and outputs on job details view
+import JobDetailsInfoView from './views/JobDetailsInfoView';
+wrap(girder.plugins.jobs.views.JobDetailsWidget, 'render', function (render) {
+    render.call(this);
+
+    if (this.job.has('workerTaskBindings')) {
+        var el = $('<div/>', {class: 'g-worker-tasks-job-info-container'}).appendTo(this.$el);
+
+        new JobDetailsInfoView({
+            el,
+            parentView: this,
+            model: this.job
+        }).render();
+    }
 });
