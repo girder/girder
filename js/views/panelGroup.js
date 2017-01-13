3c71f8afadf830562bfa824c16a0c7e006494139
Brian Helba <brian.helba@kitware.com>
2017-01-13 10:28:46 -0500

    Fix a bug where the client was parsing some XML REST responses as JSON

--- js/views/panelGroup.js
+++ js/views/panelGroup.js
@@ -183,7 +183,8 @@ slicer.views.PanelGroup = girder.View.extend({
      */
     setAnalysis: function (path) {
         return girder.restRequest({
-            path: path + '/xmlspec'
+            path: path + '/xmlspec',
+            dataType: 'xml'
         }).then(_.bind(function (xml) {
             this._submit = path + '/run';
             this._schema(xml);
