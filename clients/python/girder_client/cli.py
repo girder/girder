1ed2909f8033d03bf513f101f9c94edf375f05ae
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2017-03-02 15:49:47 -0500

    girder_client: Change order of option to be consistent with recommendation

--- clients/python/girder_client/cli.py
+++ clients/python/girder_client/cli.py
@@ -53,13 +53,13 @@ CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
 
 @click.group(context_settings=CONTEXT_SETTINGS)
-@click.option('--username', default=None)
-@click.option('--password', default=None)
 @click.option('--api-key', default=None)
+@click.option('--host', default=GirderClient.DEFAULT_HOST, show_default=True)
 @click.option('--api-url', default=None,
               help='full URL to the RESTful API of a Girder server')
+@click.option('--username', default=None)
+@click.option('--password', default=None)
 @click.option('--scheme', default=GirderClient.DEFAULT_SCHEME, show_default=True)
-@click.option('--host', default=GirderClient.DEFAULT_HOST, show_default=True)
 @click.option('--port', default=GirderClient.DEFAULT_PORT, show_default=True)
 @click.option('--api-root', default=GirderClient.DEFAULT_API_ROOT,
               help='relative path to the Girder REST API', show_default=True)
