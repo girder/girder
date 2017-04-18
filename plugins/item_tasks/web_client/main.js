5ef06b41cd1a40aa0d45667e0a2882ebed86fa46
Zach Mullen <zach.mullen@kitware.com>
2017-04-18 10:55:06 -0400

    Add job provenance to output items, and show job link in item UI

--- plugins/item_tasks/web_client/main.js
+++ plugins/item_tasks/web_client/main.js
@@ -8,7 +8,7 @@ import GlobalNavView from 'girder/views/layout/GlobalNavView';
 import HierarchyWidget from 'girder/views/widgets/HierarchyWidget';
 import ItemView from 'girder/views/body/ItemView';
 import { registerPluginNamespace } from 'girder/pluginUtils';
-
+import JobModel from 'girder_plugins/jobs/models/JobModel';
 import * as itemTasks from 'girder_plugins/item_tasks';
 
 registerPluginNamespace('item_tasks', itemTasks);
@@ -25,13 +25,23 @@ wrap(GlobalNavView, 'initialize', function (initialize) {
 });
 
 import itemMenuModTemplate from './templates/itemMenuMod.pug';
+import itemInfoModTemplate from './templates/itemInfoMod.pug';
 wrap(ItemView, 'render', function (render) {
     this.once('g:rendered', function () {
         this.$('.g-item-actions-menu').prepend(itemMenuModTemplate({
-            _: _,
+            _,
             item: this.model,
             currentUser: getCurrentUser()
         }));
+
+        if (this.model.get('createdByJob')) {
+            var job = new JobModel({_id: this.model.get('createdByJob')});
+            job.fetch().done(() => {
+                this.$('.g-item-info').append(itemInfoModTemplate({
+                    job
+                }));
+            });
+        }
     }, this);
     return render.call(this);
 });
