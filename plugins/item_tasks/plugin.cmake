dc984254b881053316d42a476909f40160a03376
Dan LaManna <dan.lamanna@kitware.com>
2017-05-10 11:40:29 -0400

    Add ansible tests for item_tasks plugin
    This should exercise the ability of vagrant up/provision/destroy the
    included Vagrantfile for the item tasks plugin. These tests are only
    added if CMake is run with ANSIBLE_TESTS=ON as they are long running
    and shouldn't be run during typical CI.

--- plugins/item_tasks/plugin.cmake
+++ plugins/item_tasks/plugin.cmake
@@ -15,3 +15,34 @@ add_web_client_test(
 
 add_eslint_test(${PLUGIN} "${PROJECT_SOURCE_DIR}/plugins/${PLUGIN}/web_client")
 add_puglint_test(${PLUGIN} "${PROJECT_SOURCE_DIR}/plugins/${PLUGIN}/web_client/templates")
+
+
+if(ANSIBLE_TESTS)
+  find_program(VAGRANT_EXECUTABLE vagrant)
+
+  add_test(NAME "${PLUGIN}.vagrant_up"
+    WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}/plugins/${PLUGIN}/devops"
+    COMMAND "${VAGRANT_EXECUTABLE}" "up" "--no-provision")
+  set_tests_properties("${PLUGIN}.vagrant_up" PROPERTIES
+    RUN_SERIAL ON
+    LABELS girder_ansible)
+
+  add_test(NAME "${PLUGIN}.vagrant_provision"
+    WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}/plugins/${PLUGIN}/devops"
+    COMMAND ${VAGRANT_EXECUTABLE} "provision")
+  set_tests_properties("${PLUGIN}.vagrant_provision"
+    PROPERTIES
+    FAIL_REGULAR_EXPRESSION "VM not created."
+    RUN_SERIAL ON
+    DEPENDS "${PLUGIN}.vagrant_up"
+    LABELS girder_ansible)
+
+  add_test(NAME "${PLUGIN}.vagrant_destroy"
+    WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}/plugins/${PLUGIN}/devops"
+    COMMAND "${VAGRANT_EXECUTABLE}" "destroy" "-f")
+  set_tests_properties("${PLUGIN}.vagrant_destroy" PROPERTIES
+    DEPENDS "${PLUGIN}.vagrant_up;${PLUGIN}.vagrant_provision"
+    RUN_SERIAL ON
+    FAIL_REGULAR_EXPRESSION "Host file not found;no hosts matched"
+    LABELS girder_ansible)
+endif()
