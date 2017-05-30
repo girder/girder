974fdbfc66323f05e34590110eb44b1dbae12d31
Brian Helba <brian.helba@kitware.com>
2017-05-30 16:33:13 -0400

    Add a static analysis test for the dicom_viewer plugin's Webpack helper

--- plugins/dicom_viewer/plugin.cmake
+++ plugins/dicom_viewer/plugin.cmake
@@ -1,1 +1,6 @@
 add_standard_plugin_tests()
+
+set(_pluginDir "${CMAKE_CURRENT_LIST_DIR}")
+get_filename_component(_pluginName "${CMAKE_CURRENT_LIST_DIR}" NAME)
+
+add_eslint_test(${_pluginName}_webpack "${_pluginDir}/webpack.helper.js")
