e9f271b1bb56d17da94109bc0f802d265220684b
Brian Helba <brian.helba@kitware.com>
2017-05-30 16:07:10 -0400

    Define most plugin tests with add_standard_plugin_tests

--- plugins/candela/plugin.cmake
+++ plugins/candela/plugin.cmake
@@ -1,9 +1,1 @@
-get_filename_component(PLUGIN ${CMAKE_CURRENT_LIST_DIR} NAME)
-
-add_web_client_test(${PLUGIN}
-    "${PROJECT_SOURCE_DIR}/plugins/${PLUGIN}/plugin_tests/candelaSpec.js"
-    PLUGIN ${PLUGIN})
-add_eslint_test(
-    ${PLUGIN} "${PROJECT_SOURCE_DIR}/plugins/${PLUGIN}/web_client")
-add_puglint_test(${PLUGIN}
-    "${PROJECT_SOURCE_DIR}/plugins/${PLUGIN}/web_client/templates")
+add_standard_plugin_tests()
--- plugins/celery_jobs/plugin.cmake
+++ plugins/celery_jobs/plugin.cmake
@@ -1,9 +1,1 @@
-get_filename_component(PLUGIN ${CMAKE_CURRENT_LIST_DIR} NAME)
-
-add_python_style_test(python_static_analysis_${PLUGIN}
-                      "${PROJECT_SOURCE_DIR}/plugins/${PLUGIN}/server")
-
-add_eslint_test(${PLUGIN}
-    "${PROJECT_SOURCE_DIR}/plugins/${PLUGIN}/web_client")
-add_puglint_test(${PLUGIN}
-    "${PROJECT_SOURCE_DIR}/plugins/${PLUGIN}/web_client/templates")
+add_standard_plugin_tests()
