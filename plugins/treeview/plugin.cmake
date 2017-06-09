b9029433387ed4007550c9ef9de7821c357d4d37
Jonathan Beezley <jonathan.beezley@kitware.com>
2017-06-09 09:09:50 -0400

    Use cmake plugin macro to define tests

--- plugins/treeview/plugin.cmake
+++ plugins/treeview/plugin.cmake
@@ -1,8 +1,1 @@
-get_filename_component(PLUGIN ${CMAKE_CURRENT_LIST_DIR} NAME)
-
-add_web_client_test(
-    types "${PROJECT_SOURCE_DIR}/plugins/${PLUGIN}/plugin_tests/types.js" PLUGIN ${PLUGIN})
-
-add_eslint_test(${PLUGIN} "${PROJECT_SOURCE_DIR}/plugins/${PLUGIN}/web_client")
-add_eslint_test(${PLUGIN}-tests "${PROJECT_SOURCE_DIR}/plugins/${PLUGIN}/plugin_tests")
-add_puglint_test(${PLUGIN} "${PROJECT_SOURCE_DIR}/plugins/${PLUGIN}/web_client/templates")
+add_standard_plugin_tests(NO_SERVER)
