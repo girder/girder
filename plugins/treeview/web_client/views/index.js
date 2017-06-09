22c9e0ab626f0954a23093ec71ea98ea33d139d1
Jonathan Beezley <jonathan.beezley@kitware.com>
2017-06-09 08:38:49 -0400

    Add all views to the plugin namespace

--- plugins/treeview/web_client/views/index.js
+++ plugins/treeview/web_client/views/index.js
@@ -1,7 +1,15 @@
-import TreeView from './TreeView';
+import ConfigView from './ConfigView';
+import FileDialog from './FileDialog';
+import FolderDialog from './FolderDialog';
+import ItemDialog from './ItemDialog';
 import TreeDialog from './TreeDialog';
+import TreeView from './TreeView';
 
 export {
-    TreeView,
-    TreeDialog
+    ConfigView,
+    FileDialog,
+    FolderDialog,
+    ItemDialog,
+    TreeDialog,
+    TreeView
 };
