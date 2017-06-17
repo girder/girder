10b119a83512c5311a4a28c86855753f2888b248
Jonathan Beezley <jonathan.beezley@kitware.com>
2017-06-17 15:33:48 -0400

    Work around problems with girder's rest api

--- plugins/backbone/web_client/models/Model.js
+++ plugins/backbone/web_client/models/Model.js
@@ -8,7 +8,12 @@ export default Backbone.Model.extend({
     resource: null,
     parent: _.constant(null),
     children: _.constant([]),
-    url() {
-        return `${apiRoot}/${this.resource}/this.id`;
+    urlRoot() {
+        return `${apiRoot}/${this.resource}`;
+    },
+    sync(method, model, options = {}) {
+        options.data = model.attributes;
+        options.processData = true;
+        return Backbone.sync.apply(this, arguments);
     }
 });
