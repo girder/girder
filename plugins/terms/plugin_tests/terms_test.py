42b6b6189a795ed50cddd382932dc0c9d807b5ad
Brian Helba <brian.helba@kitware.com>
2017-07-17 21:50:36 -0400

    Add a Terms of Use plugin

---
+++ plugins/terms/plugin_tests/terms_test.py
@@ -0,0 +1,177 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+###############################################################################
+#  Copyright Kitware Inc.
+#
+#  Licensed under the Apache License, Version 2.0 ( the "License" );
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+###############################################################################
+
+from girder.constants import SettingKey
+
+from tests import base
+
+
+def setUpModule():
+    base.enabledPlugins.append('terms')
+    base.startServer()
+
+
+def tearDownModule():
+    base.stopServer()
+
+
+class TermsTest(base.TestCase):
+    def setUp(self):
+        base.TestCase.setUp(self)
+
+        self.siteAdminUser = self.model('user').createUser(
+            email='rocky@phila.pa.us',
+            login='rocky',
+            firstName='Robert',
+            lastName='Balboa',
+            password='adrian'
+        )
+        self.creatorUser = self.model('user').createUser(
+            email='creed@la.ca.us',
+            login='creed',
+            firstName='Apollo',
+            lastName='Creed',
+            password='the1best'
+        )
+        creationSetting = self.model('setting').getDefault(SettingKey.COLLECTION_CREATE_POLICY)
+        creationSetting['open'] = True
+        self.model('setting').set(SettingKey.COLLECTION_CREATE_POLICY, creationSetting)
+
+    def testTerms(self):
+        # Ensure that ordinary collections still work
+        resp = self.request('/collection', method='POST', user=self.creatorUser, params={
+            'name': 'Basic Collection',
+            'description': 'Some description.',
+            'public': True
+        })
+        self.assertStatusOk(resp)
+        self.assertDictContainsSubset({
+            'name': 'Basic Collection',
+            'description': 'Some description.',
+            'public': True,
+            'size': 0,
+            '_modelType': 'collection',
+            'terms': None
+        }, resp.json)
+        basicCollectionId = resp.json['_id']
+
+        # Try to accept the terms, on a collection with no terms
+        resp = self.request(
+            '/collection/%s/acceptTerms' % basicCollectionId, method='POST', user=self.creatorUser,
+            params={
+                # This is the hash of an empty string
+                'termsHash': '47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU='
+            })
+        self.assertStatus(resp, 400)
+
+        # Create a collection with unicode terms
+        resp = self.request('/collection', method='POST', user=self.creatorUser, params={
+            'name': 'Terms Collection',
+            'description': 'Some other description.',
+            'public': True,
+            'terms': u'# Sample Terms of Use\n\n**\u00af\\\\\\_(\u30c4)\\_/\u00af**'.encode('utf-8')
+        })
+        self.assertStatusOk(resp)
+        self.assertDictContainsSubset({
+            'name': 'Terms Collection',
+            'description': 'Some other description.',
+            'public': True,
+            'size': 0,
+            '_modelType': 'collection',
+            'terms': u'# Sample Terms of Use\n\n**\u00af\\\\\\_(\u30c4)\\_/\u00af**'
+        }, resp.json)
+        termsCollectionId = resp.json['_id']
+
+        # Fetch the terms on a collection, ensuring they're saved
+        resp = self.request(
+            '/collection/%s' % termsCollectionId, method='GET', user=self.creatorUser)
+        self.assertStatusOk(resp)
+        self.assertDictContainsSubset({
+            'name': 'Terms Collection',
+            'description': 'Some other description.',
+            'public': True,
+            'size': 0,
+            '_modelType': 'collection',
+            'terms': u'# Sample Terms of Use\n\n**\u00af\\\\\\_(\u30c4)\\_/\u00af**'
+        }, resp.json)
+
+        # Ensure that the user has not yet accepted any terms
+        resp = self.request('/user/me', method='GET', user=self.creatorUser)
+        self.assertStatusOk(resp)
+        self.assertNotHasKeys(resp.json, {'terms'})
+
+        # Try to accept the terms, with the wrong hash
+        resp = self.request(
+            '/collection/%s/acceptTerms' % termsCollectionId, method='POST', user=self.creatorUser,
+            params={
+                'termsHash': 'gargTz1mz476PQf9oUVbtob7OS3ban/3aHqOdLgcHA0='.replace('g', 'f')
+            })
+        self.assertStatus(resp, 400)
+
+        # Accept the terms
+        resp = self.request(
+            '/collection/%s/acceptTerms' % termsCollectionId, method='POST', user=self.creatorUser,
+            params={
+                'termsHash': 'gargTz1mz476PQf9oUVbtob7OS3ban/3aHqOdLgcHA0='
+            })
+        self.assertStatusOk(resp)
+
+        # Check that the user has accepted the terms
+        resp = self.request('/user/me', method='GET', user=self.creatorUser)
+        self.assertStatusOk(resp)
+        self.assertDictContainsSubset({
+            'terms': {
+                'collection': {
+                    termsCollectionId: 'gargTz1mz476PQf9oUVbtob7OS3ban/3aHqOdLgcHA0='
+                }
+            }},
+            resp.json)
+
+        # Ensure that other users cannot read term acceptances
+        resp = self.request('/user/%s' % self.creatorUser['_id'], method='GET')
+        self.assertStatusOk(resp)
+        self.assertNotHasKeys(resp.json, {'terms'})
+
+        # Modify a collection to have new terms
+        resp = self.request(
+            '/collection/%s' % termsCollectionId, method='PUT', user=self.creatorUser, params={
+                'description': 'A new description.',
+                'terms': '# New Terms of Use\n\nThese have changed.'
+            })
+        self.assertStatusOk(resp)
+        self.assertDictContainsSubset({
+            'name': 'Terms Collection',
+            'description': 'A new description.',
+            'public': True,
+            'size': 0,
+            '_modelType': 'collection',
+            'terms': '# New Terms of Use\n\nThese have changed.'
+        }, resp.json)
+
+        # Fetch the terms on a collection, while anonymous
+        resp = self.request('/collection/%s' % termsCollectionId, method='GET')
+        self.assertStatusOk(resp)
+        self.assertDictContainsSubset({
+            'name': 'Terms Collection',
+            'description': 'A new description.',
+            'public': True,
+            'size': 0,
+            '_modelType': 'collection',
+            'terms': '# New Terms of Use\n\nThese have changed.'
+        }, resp.json)
---
+++ plugins/terms/server/__init__.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+###############################################################################
+#  Copyright Kitware Inc.
+#
+#  Licensed under the Apache License, Version 2.0 ( the "License" );
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+###############################################################################
+
+import base64
+import hashlib
+
+from girder import events
+from girder.api import access
+from girder.api.describe import Description, autoDescribeRoute
+from girder.api.rest import boundHandler, RestException
+from girder.api.v1.collection import Collection
+from girder.constants import AccessType, TokenScope
+from girder.models.model_base import ModelImporter
+
+
+@access.user(scope=TokenScope.DATA_READ)
+@boundHandler
+@autoDescribeRoute(
+    Description('Accept a collection\'s Terms of Use for the current user.')
+    .modelParam('id', model='collection', level=AccessType.READ)
+    .param('termsHash', 'The SHA-256 hash of this collection\'s terms, encoded in Base64.')
+)
+def acceptCollectionTerms(self, collection, termsHash):
+    if not collection.get('terms'):
+        raise RestException('This collection currently has no terms.')
+
+    # termsHash should be encoded to a bytes object, but storing bytes into MongoDB behaves
+    # differently in Python 2 vs 3. Additionally, serializing a bytes to JSON behaves differently
+    # in Python 2 vs 3. So, just keep it as a unicode (or ordinary Python 2 str).
+    realTermsHash = base64.b64encode(
+        hashlib.sha256(collection['terms'].encode('utf-8')).digest()
+    ).decode('utf-8')
+    if termsHash != realTermsHash:
+        # This "proves" that the client has at least accessed the terms
+        raise RestException(
+            'The submitted "termsHash" does not correspond to the collection\'s current terms.')
+
+    ModelImporter.model('user').update(
+        {'_id': self.getCurrentUser()['_id']},
+        {'$set': {'terms.collection.%s' % collection['_id']: termsHash}}
+    )
+
+
+def afterPostPutCollection(event):
+    # This will only trigger if no exceptions (for access, invalid id, etc.) are thrown
+    extraParams = event.info['params']
+    if 'terms' in extraParams:
+        collectionResponse = event.info['returnVal']
+        collectionId = collectionResponse['_id']
+        terms = extraParams['terms']
+
+        ModelImporter.model('collection').update(
+            {'_id': collectionId},
+            {'$set': {'terms': terms}}
+        )
+
+        collectionResponse['terms'] = terms
+        event.addResponse(collectionResponse)
+
+
+def load(info):
+    # Augment the collection creation and edit routes to accept a terms field
+    events.bind('rest.post.collection.after', 'terms', afterPostPutCollection)
+    events.bind('rest.put.collection/:id.after', 'terms', afterPostPutCollection)
+    for handler in [
+        Collection.createCollection,
+        Collection.updateCollection
+    ]:
+        handler.description.param('terms', 'The Terms of Use for the collection.', required=False)
+
+    # Expose the terms field on all collections
+    ModelImporter.model('collection').exposeFields(level=AccessType.READ, fields={'terms'})
+
+    # Add endpoint for registered users to accept terms
+    info['apiRoot'].collection.route('POST', (':id', 'acceptTerms'), acceptCollectionTerms)
+
+    # Expose the terms field on all users
+    ModelImporter.model('user').exposeFields(level=AccessType.ADMIN, fields={'terms'})
