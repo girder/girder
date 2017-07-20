5708cd21f72f2fd64f59ebb32a8dde331ebbabc5
Jeffrey Baumes <jeff.baumes@kitware.com>
2017-07-20 15:15:55 -0400

    Add support for candela vis of tab-delimited files

--- plugins/candela/web_client/views/CandelaWidget.js
+++ plugins/candela/web_client/views/CandelaWidget.js
@@ -43,61 +43,77 @@ var CandelaWidget = View.extend({
     },
 
     render: function () {
-        if (this.accessLevel >= AccessType.READ && this.item.get('name').toLowerCase().endsWith('.csv')) {
-            this.$el.html(CandelaWidgetTemplate({
-                components: this._components
-            }));
-            this.parametersView.setElement($('.g-item-candela-parameters'));
-            datalib.csv(this.item.downloadUrl(), (error, data) => {
-                if (error) {
-                    let info = {
-                        text: 'An error occurred while attempting to read and ' +
-                              'parse the data file. Details have been logged in the console.',
-                        type: 'danger',
-                        timeout: 5000,
-                        icon: 'attention'
-                    };
-                    events.trigger('g:alert', info);
-                    console.error(error);
-                    return;
-                }
-
-                datalib.read(data, {parse: 'auto'});
-
-                // Vega has issues with empty-string fields and fields with dots, so rename those.
-                let rename = [];
-                for (let key in data.__types__) {
-                    if (data.__types__.hasOwnProperty(key)) {
-                        if (key === '') {
-                            rename.push(['', 'id']);
-                        } else if (key.indexOf('.') >= 0) {
-                            rename.push([key, key.replace(/\./g, '_')]);
-                        }
-                    }
-                }
-
-                rename.forEach((d) => {
-                    data.__types__[d[1]] = data.__types__[d[0]];
-                    delete data.__types__[d[0]];
-                    data.forEach((row) => {
-                        row[d[1]] = row[d[0]];
-                        delete row[d[0]];
-                    });
-                });
-
-                let columns = [];
-                for (let key in data.__types__) {
-                    if (data.__types__.hasOwnProperty(key)) {
-                        columns.push(key);
-                    }
-                }
-                this.parametersView.setData(data, columns);
-                this.updateComponent();
-            });
-        } else {
+        let cleanup = function () {
             this.$('.g-item-candela')
                 .remove();
+        };
+
+        if (this.accessLevel < AccessType.READ) {
+            cleanup();
+            return;
         }
+
+        let parser = null;
+        let name = this.item.get('name').toLowerCase();
+        if (name.endsWith('.csv')) {
+            parser = datalib.csv;
+        } else if (name.endsWith('.tsv') || name.endsWith('.tab')) {
+            parser = datalib.tsv;
+        } else {
+            cleanup();
+            return;
+        }
+
+        this.$el.html(CandelaWidgetTemplate({
+            components: this._components
+        }));
+        this.parametersView.setElement($('.g-item-candela-parameters'));
+        parser(this.item.downloadUrl(), (error, data) => {
+            if (error) {
+                let info = {
+                    text: 'An error occurred while attempting to read and ' +
+                          'parse the data file. Details have been logged in the console.',
+                    type: 'danger',
+                    timeout: 5000,
+                    icon: 'attention'
+                };
+                events.trigger('g:alert', info);
+                console.error(error);
+                return;
+            }
+
+            datalib.read(data, {parse: 'auto'});
+
+            // Vega has issues with empty-string fields and fields with dots, so rename those.
+            let rename = [];
+            for (let key in data.__types__) {
+                if (data.__types__.hasOwnProperty(key)) {
+                    if (key === '') {
+                        rename.push(['', 'id']);
+                    } else if (key.indexOf('.') >= 0) {
+                        rename.push([key, key.replace(/\./g, '_')]);
+                    }
+                }
+            }
+
+            rename.forEach((d) => {
+                data.__types__[d[1]] = data.__types__[d[0]];
+                delete data.__types__[d[0]];
+                data.forEach((row) => {
+                    row[d[1]] = row[d[0]];
+                    delete row[d[0]];
+                });
+            });
+
+            let columns = [];
+            for (let key in data.__types__) {
+                if (data.__types__.hasOwnProperty(key)) {
+                    columns.push(key);
+                }
+            }
+            this.parametersView.setData(data, columns);
+            this.updateComponent();
+        });
     }
 });
 
