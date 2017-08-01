93d5be475384778eabea564ffdb2a5f729648af1
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2017-08-01 10:19:35 -0500

    Use regexes instead of string comparisons in tests

--- clients/web/test/spec/sortWidgetSpec.js
+++ clients/web/test/spec/sortWidgetSpec.js
@@ -33,16 +33,18 @@ describe('Sort user list', function () {
         girderTest.goToUsersPage()();
         runs(function () {
             expect($('.g-user-list-entry').length).toBe(3);
-            expect($('a.g-user-link').text()).toBe('AFirstName ALastNameBFirstName BLastNameCFirstName CLastName');
+            var defaultOrder = /.*AFirstName.*ALastName.*BFirstName.*BLastName.*CFirstName.*CLastName.*/;
+            expect($('a.g-user-link').text()).toMatch(defaultOrder);
             $('a.g-sort-order-button:not(.hide)').click();
         });
 
         waitsFor(function () {
             return $('a.g-sort-order-button.g-down:not(.hide)').length > 0;
-        }, 'switching sort icon');
+        }, 'switching sort order via icon');
 
         runs(function () {
-            expect($('a.g-user-link').text()).toBe('CFirstName CLastNameBFirstName BLastNameAFirstName ALastName');
+            var reversedDefaultOrder = /.*CFirstName.*CLastName.*BFirstName.*BLastName.*AFirstName.*ALastName.*/;
+            expect($('a.g-user-link').text()).toMatch(reversedDefaultOrder);
         });
 
         runs(function () {
@@ -54,18 +56,20 @@ describe('Sort user list', function () {
         }, 'wait for member drop down dialog to appear');
 
         runs(function () {
-            expect($('a.g-collection-sort-link').text()).toBe('Last NameCreation DateUsed Space');
+            expect($('a.g-collection-sort-link:contains("Creation Date")').length).toBe(1);
             $('a.g-collection-sort-link:contains("Creation Date")').click();
         });
 
         waitsFor(function () {
             // FIXME: the condition here should be: wait for re-render of collection,
             //   but I don't know how to express that...
-            return $('.g-user-link').text() === 'AFirstName ALastNameCFirstName CLastNameBFirstName BLastName';
+            var sortedByCreationDate = /.*AFirstName.*ALastName.*CFirstName.*CLastName.*BFirstName.*BLastName.*/;
+            return sortedByCreationDate.test($('.g-user-link').text());
         }, 'refetching and rendering user list');
 
         runs(function () {
-            expect($('a.g-user-link').text()).toBe('AFirstName ALastNameCFirstName CLastNameBFirstName BLastName');
+            var sortedByCreationDate = /.*AFirstName.*ALastName.*CFirstName.*CLastName.*BFirstName.*BLastName.*/;
+            expect($('a.g-user-link').text()).toMatch(sortedByCreationDate);
         });
     });
 });
