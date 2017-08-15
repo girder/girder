a0e97ad67b035c825f90581b83006ceccacde0e4
Brian Helba <brian.helba@kitware.com>
2017-08-15 14:58:58 -0400

    Use grunt-contrib-pug to build test HTML

--- Gruntfile.js
+++ Gruntfile.js
@@ -84,6 +84,7 @@ module.exports = function (grunt) {
     }
 
     grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-pug');
     grunt.loadNpmTasks('grunt-contrib-stylus');
     grunt.loadNpmTasks('grunt-contrib-symlink');
     grunt.loadNpmTasks('grunt-contrib-uglify');
