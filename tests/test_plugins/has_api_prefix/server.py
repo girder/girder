cc19d8ead9e4088b02e333828d6b91a2c4e690e8
Chris Harris <chris.harris@kitware.com>
2017-10-25 15:20:26 -0400

    Fix flake8

--- tests/test_plugins/has_api_prefix/server.py
+++ tests/test_plugins/has_api_prefix/server.py
@@ -34,6 +34,7 @@ class Resourceful(Resource):
     def getResource(self, params):
         return ['custom REST route']
 
+
 def load(info):
     info['apiRoot'].prefix = Prefix()
     info['apiRoot'].prefix.resourceful = Resourceful()
