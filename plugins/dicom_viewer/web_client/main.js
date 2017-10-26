beebc1154404379acb4c378a6ddcf9d62596a228
Pierre Assemat <pierre.assemat@kitware.com>
2017-10-26 13:54:03 -0400

    Add a button to force DICOM extraction

--- plugins/dicom_viewer/web_client/main.js
+++ plugins/dicom_viewer/web_client/main.js
@@ -1,11 +1,23 @@
+import { getCurrentUser } from 'girder/auth';
+import { AccessType } from 'girder/constants';
+import events from 'girder/events';
 import { restRequest } from 'girder/rest';
 import { wrap } from 'girder/utilities/PluginUtils';
 import ItemView from 'girder/views/body/ItemView';
 
 import DicomItemView from './views/DicomView';
+import ParseDicomItemTemplate from './templates/parseDicomItem.pug';
 
 wrap(ItemView, 'render', function (render) {
     this.once('g:rendered', () => {
+        // Add a button to force DICOM extraction
+        if (this.model.get('_accessLevel') >= AccessType.WRITE) {
+            this.$('.g-item-actions-menu').prepend(ParseDicomItemTemplate({
+                item: this.model,
+                currentUser: getCurrentUser()
+            }));
+        }
+
         restRequest({
             url: `item/${this.model.id}/dicom`,
             data: {
@@ -25,3 +37,28 @@ wrap(ItemView, 'render', function (render) {
     });
     return render.call(this);
 });
+
+ItemView.prototype.events['click .g-dicom-parse-item'] = function () {
+    restRequest({
+        method: 'POST',
+        url: `item/${this.model.id}/parseDicom`,
+        error: null
+    })
+        .done((resp) => {
+            // Show up a message to alert the user it was done
+            events.trigger('g:alert', {
+                icon: 'ok',
+                text: 'Dicom item parsed.',
+                type: 'success',
+                timeout: 4000
+            });
+        })
+        .fail((resp) => {
+            events.trigger('g:alert', {
+                icon: 'cancel',
+                text: 'No Dicom metadata.',
+                type: 'danger',
+                timeout: 4000
+            });
+        });
+};
