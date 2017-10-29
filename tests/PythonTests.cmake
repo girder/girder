809567e1960151818c85b8e64d062b7e0f384c42
Brian Helba <brian.helba@kitware.com>
2017-10-29 13:01:45 -0400

    Set an accurate CTest label for style tests

--- tests/PythonTests.cmake
+++ tests/PythonTests.cmake
@@ -72,7 +72,7 @@ function(add_python_style_test name input)
       WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}"
       COMMAND "${FLAKE8_EXECUTABLE}" "--config=${flake8_config}" "${input}"
     )
-    set_property(TEST "${name}" PROPERTY LABELS girder_static_analysis)
+    set_property(TEST "${name}" PROPERTY LABELS girder_python)
   endif()
 endfunction()
 
