ba8cdebcebfd5827fafebecd1016ab1325005b9b
Pierre Assemat <pierre.assemat@kitware.com>
2017-11-03 15:54:51 -0400

    Client: Select search mode and query by 'PaginateWidget'
    The goal is to allow the 'PaginateWidget' to search
    in a Collection with a specific query and a specific search
    mode.
    
    The search mode has to be referenced in the Search Registry.

--- clients/web/src/views/widgets/PaginateWidget.js
+++ clients/web/src/views/widgets/PaginateWidget.js
@@ -5,19 +5,23 @@ import PaginateWidgetTemplate from 'girder/templates/widgets/paginateWidget.pug'
 /**
  * This widget is used to provide a consistent widget for iterating amongst
  * pages of a Collection.
+ * The parameter "fetchParams" is use to specify a "query" and a "mode" fields
+ * in order to do a specific search into a Collection.
+ * This is used to perform pagination in the 'SearchResult' view
  */
 var PaginateWidget = View.extend({
     events: {
         'click .g-page-next:not(.disabled)': function (e) {
-            this.collection.fetchNextPage();
+            this.collection.fetchNextPage(this.fetchParams);
         },
         'click .g-page-prev:not(.disabled)': function (e) {
-            this.collection.fetchPreviousPage();
+            this.collection.fetchPreviousPage(this.fetchParams);
         }
     },
 
     initialize: function (settings) {
         this.collection = settings.collection;
+        this.fetchParams = settings.fetchParams || {};
     },
 
     /**
