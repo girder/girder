150fb8fd06c350e27a7b3591417ec31daec55089
Zach Mullen <zach.mullen@kitware.com>
2017-12-14 16:46:27 -0500

    Port route table test to pytest
    This required one change to the server fixture when loading
    plugins -- we needed to ensure that the setting value for
    plugins enabled matched the set of plugins we used when
    calling server.setup(), since the route table logic inspects
    the old value of that setting.

---
+++ girder/test/test_route_table.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+###############################################################################
+#  Copyright 2016 Kitware Inc.
+#
+#  Licensed under the Apache License, Version 2.0 ( the "License" );
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+###############################################################################
+
+import json
+import pytest
+from girder.constants import GIRDER_ROUTE_ID, GIRDER_STATIC_ROUTE_ID, SettingKey
+from girder.exceptions import ValidationException
+from girder.models.setting import Setting
+from pytest_girder.assertions import assertStatusOk
+from pytest_girder.utils import getResponseBody
+
+
+@pytest.mark.parametrize('value,err', [
+    ({}, r'Girder and static root must be routable\.$'),
+    ({GIRDER_ROUTE_ID: '/'}, r'Girder and static root must be routable\.$'),
+    ({
+        GIRDER_ROUTE_ID: '/some_route',
+        GIRDER_STATIC_ROUTE_ID: '/static',
+        'other': '/some_route'
+    }, r'Routes must be unique\.$'),
+    ({
+        GIRDER_ROUTE_ID: '/',
+        GIRDER_STATIC_ROUTE_ID: '/static',
+        'other': 'route_without_a_leading_slash'
+    }, r'Routes must begin with a forward slash\.$'),
+    ({
+        GIRDER_ROUTE_ID: '/',
+        GIRDER_STATIC_ROUTE_ID: 'relative/static'
+    }, r'Static root must begin with a forward slash or contain a URL scheme\.$')
+])
+def testRouteTableValidationFailure(value, err):
+    with pytest.raises(ValidationException, match=err):
+        Setting().validate({
+            'key': SettingKey.ROUTE_TABLE,
+            'value': value
+        })
+
+
+@pytest.mark.parametrize('value', [
+    {
+        GIRDER_ROUTE_ID: '/',
+        GIRDER_STATIC_ROUTE_ID: 'http://127.0.0.1/static'
+    }
+])
+def testRouteTableValidationSuccess(value):
+    Setting().validate({
+        'key': SettingKey.ROUTE_TABLE,
+        'value': value
+    })
+
+
+@pytest.mark.testPlugins(['has_webroot'])
+def testRouteTableBehavior(server, admin):
+    Setting().set(SettingKey.ROUTE_TABLE, {
+        GIRDER_ROUTE_ID: '/',
+        GIRDER_STATIC_ROUTE_ID: '/static',
+        'has_webroot': '/has_webroot'
+    })
+
+    # /has_webroot should serve our plugin webroot
+    resp = server.request('/has_webroot', prefix='', isJson=False, appPrefix='/has_webroot')
+    assertStatusOk(resp)
+    assert 'some webroot' in getResponseBody(resp)
+
+    # girder should be at /
+    resp = server.request('/', prefix='', isJson=False)
+    assertStatusOk(resp)
+    assert 'g-global-info-apiroot' in getResponseBody(resp)
+
+    table = Setting().get(SettingKey.ROUTE_TABLE)
+    assert 'has_webroot' in table
+    assert table['has_webroot'] == '/has_webroot'
+
+    # Disabling the plugin should remove it from the route table
+    assertStatusOk(server.request('/system/plugins', method='PUT', user=admin, params={
+        'plugins': json.dumps([])
+    }))
+    table = Setting().get(SettingKey.ROUTE_TABLE)
+    assert 'has_webroot' not in table
