0b81e77a548075e732a010ed65ed16c2b8d49967
David Manthey <david.manthey@kitware.com>
2017-12-15 10:55:57 -0500

    Change the default, not the ini settings.

--- pytest_girder/pytest_girder/plugin.py
+++ pytest_girder/pytest_girder/plugin.py
@@ -8,5 +8,5 @@ def pytest_addoption(parser):
     group.addoption('--mongo-uri', action='store', default='mongodb://localhost:27017',
                     help=('The base URI to the MongoDB instance to use for database connections, '
                           'default is mongodb://localhost:27017'))
-    group.addoption('--drop-db', action='store', default='pre', choices=('pre', 'post', 'never'),
-                    help='When to destroy testing databases, default is pre (before running tests)')
+    group.addoption('--drop-db', action='store', default='post', choices=('pre', 'post', 'never'),
+                    help='When to destroy testing databases, default is post (after running tests)')
