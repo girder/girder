f1fde7344f5daa56ff7d7b8858c9b5fd322e75d7
Zach Mullen <zach.mullen@kitware.com>
2018-01-22 21:26:33 -0500

    Port path utils test to pytest

---
+++ test/test_path_utilities.py
@@ -0,0 +1,35 @@
+import pytest
+from girder.utility import path
+
+
+@pytest.mark.parametrize('raw,encoded', [
+    ('abcd', 'abcd'),
+    ('/', '\/'),
+    ('\\', '\\\\'),
+    ('/\\', '\/\\\\'),
+    ('\\//\\', '\\\\\/\/\\\\'),
+    ('a\\\\b//c\\d', 'a\\\\\\\\b\/\/c\\\\d')
+])
+def testCodec(raw, encoded):
+    assert path.encode(raw) == encoded
+    assert path.decode(encoded) == raw
+
+
+@pytest.mark.parametrize('pth,tokens', [
+    ('abcd', ['abcd']),
+    ('/abcd', ['', 'abcd']),
+    ('/ab/cd/ef/gh', ['', 'ab', 'cd', 'ef', 'gh']),
+    ('/ab/cd//', ['', 'ab', 'cd', '', '']),
+    ('ab\\/cd', ['ab/cd']),
+    ('ab\/c/d', ['ab/c', 'd']),
+    ('ab\//cd', ['ab/', 'cd']),
+    ('ab/\/cd', ['ab', '/cd']),
+    ('ab\\\\/cd', ['ab\\', 'cd']),
+    ('ab\\\\/\\\\cd', ['ab\\', '\\cd']),
+    ('ab\\\\\\/\\\\cd', ['ab\\/\\cd']),
+    ('/\\\\abcd\\\\/', ['', '\\abcd\\', '']),
+    ('/\\\\\\\\/\\//\\\\', ['', '\\\\', '/', '\\'])
+])
+def testSplitAndJoin(pth, tokens):
+    assert path.split(pth) == tokens
+    assert path.join(tokens) == pth
