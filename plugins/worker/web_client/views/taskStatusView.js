c78567b3fa0ad2ee6ac29ba88645b48b77fa8437
Pierre Assemat <pierre.assemat@kitware.com>
2018-03-27 15:33:12 -0400

    Web Client: Add button in plugin settings page that routes to a new view
    This commit get relevant information about the workers and tasks associated
    It display them on the worker status view, add a button to refresh this informations
    at anytime and let the user know when it fetch the data by showing a spin icon during
    the load.

---
+++ plugins/worker/web_client/views/taskStatusView.js
@@ -0,0 +1,89 @@
+import _ from 'underscore';
+
+import View from 'girder/views/View';
+import { restRequest } from 'girder/rest';
+
+import taskStatusViewTemplate from '../templates/taskStatusView.pug';
+
+var taskStatusView = View.extend({
+    events: {
+        'click .g-worker-task-status-btn-reload': function () {
+            this._fetchWorkerStatus();
+        },
+        'click .g-worker-task-status-link': function (e) {
+            var row = e.target.parentElement;
+            var workerName = row.childNodes[0].innerText;
+            _.each(this.workers, (worker) => {
+                if (worker['name'] === workerName) {
+                    this.workerName = workerName;
+                    this.activeTaskList = worker['active'];
+                    this.render();
+                }
+            });
+        }
+    },
+
+    initialize: function () {
+        this._fetchWorkerStatus();
+    },
+
+    render: function () {
+        this.$el.html(taskStatusViewTemplate({
+            workerList: this.workers,
+            load: this.load,
+            workerName: this.workerName,
+            activeTasks: this.activeTaskList
+        }));
+
+        return this;
+    },
+
+    _fetchWorkerStatus: function () {
+        this.workers = [];
+        this.activeTaskList = [];
+        this.load = true;
+        restRequest({
+            method: 'GET',
+            url: 'worker/status'
+        }).done((resp) => {
+            this.load = false;
+            this.parseWorkerStatus(
+                resp.report,
+                resp.stats,
+                resp.ping,
+                resp.active,
+                resp.scheduled);
+            this.render();
+        });
+
+        this.render();
+    },
+
+    parseWorkerStatus: function (report, stats, ping, active, scheduled) {
+        var workers = _.keys(report);
+        var reportTmp = null;
+        var statsTmp = null;
+        var pingTmp = null;
+        _.each(workers, (worker) => {
+            if (_.has(report[worker], 'ok')) {
+                reportTmp = report[worker]['ok'];
+            }
+            if (stats[worker]['total'] !== null) {
+                statsTmp = _.values(stats[worker]['total'])[0];
+            }
+            if (_.has(ping[worker], 'ok')) {
+                pingTmp = ping[worker]['ok'];
+            }
+            this.workers.push({
+                'name': worker,
+                'report': reportTmp,
+                'stats': statsTmp | 0,
+                'ping': pingTmp,
+                'active': active[worker],
+                'scheduled': scheduled[worker]
+            });
+        });
+    }
+});
+
+export default taskStatusView;
