fc4c3801c54798e4555e109f2422b8a961c2104f
Zach Mullen <zach.mullen@kitware.com>
2018-05-07 14:38:11 -0400

    Add test for file download log event

--- test/audit_logs/test_audit_log.py
+++ test/audit_logs/test_audit_log.py
@@ -1,6 +1,10 @@
 import datetime
 import pytest
+import six
 from girder import auditLogger
+from girder.models.file import File
+from girder.models.folder import Folder
+from girder.models.upload import Upload
 
 
 @pytest.fixture
@@ -64,3 +68,27 @@ def testAuthenticatedRestRequestLogging(server, recordModel, resetLog, admin):
     assert records.count() == 1
     record = records[0]
     assert record['userId'] == admin['_id']
+
+
+@pytest.mark.plugin('audit_logs')
+def testDownloadLogging(server, recordModel, resetLog, admin, fsAssetstore):
+    folder = Folder().find({
+        'parentId': admin['_id'],
+        'name': 'Public'
+    })[0]
+    file = Upload().uploadFromFile(
+        six.BytesIO(b'hello'), size=5, name='test', parentType='folder', parent=folder,
+        user=admin, assetstore=fsAssetstore)
+
+    File().download(file, headers=False, offset=2, endByte=4)
+
+    records = recordModel.find()
+
+    assert records.count() == 1
+    record = records[0]
+    assert record['ip'] == '127.0.0.1'
+    assert record['type'] == 'file.download'
+    assert record['details']['fileId'] == file['_id']
+    assert record['details']['startByte'] == 2
+    assert record['details']['endByte'] == 4
+    assert isinstance(record['when'], datetime.datetime)
