0a0dc76c64ded5c9183465a22c9ff22657de8405
Brian Helba <brian.helba@kitware.com>
2018-05-17 02:38:25 -0400

    Refactor EventStream to be more maintainable and fix bugs with ".close"

--- clients/web/src/utilities/EventStream.js
+++ clients/web/src/utilities/EventStream.js
@@ -4,8 +4,6 @@ import Backbone from 'backbone';
 
 import { getApiRoot } from 'girder/rest';
 
-var timestamp = null;
-
 /**
  * The EventStream type wraps window.EventSource to listen to the unified
  * per-user event channel endpoint using the SSE protocol. When events are
@@ -14,103 +12,146 @@ var timestamp = null;
  * Listeners can bind to specific event types on the channel.
  */
 function EventStream(settings) {
-    var defaults = {
+    const defaults = {
         timeout: null,
         streamPath: '/notification/stream',
         _heartbeatTimeout: 5000 // in milliseconds
     };
 
+    // User-provided settings from initialization
     this.settings = _.extend(defaults, settings);
+    // Whether this is opened
+    this._opened = false;
+    // Whether this is started (if so, holds the EventSource instance)
+    this._eventSource = null;
+    // The identifier for the next heartbeat check
+    this._animationRequestID = null;
+    // The identifier for the next dead man's switch check
+    this._timeoutID = null;
 
     return _.extend(this, Backbone.Events);
 }
 
-var prototype = EventStream.prototype;
-
 /*
- * This method is used to kill the event stream socket when the girder tab is not
- * visible using window.requestAnimationFrame. It creates a dead man switch to
- * close the frame after _heartbeatTimeout milliseconds (default of 5 seconds)
- * if requestAnimationFrame is not called in that time.
+ * This method is used to stop the event stream socket when the Girder tab is not visible.
+ *
+ * It creates a dead man switch to close the frame after _heartbeatTimeout milliseconds (default of 5 seconds) if
+ * requestAnimationFrame is not called in that time.
  */
-prototype._heartbeat = function () {
-    if (!this._stopHeartbeat) {
-        window.requestAnimationFrame(() => { this._heartbeat(); });
+EventStream.prototype._heartbeat = function () {
+    // This has been called because it's the next animation frame or from an explicit startup
+
+    if (!this._opened) {
+        // This should never happen
+        console.warn('EventStream._heartbeat called on a closed stream.');
+        return;
     }
 
-    if (this._eventSource) {
-        window.clearTimeout(this._closeTimeout);
-        this._closeTimeout = window.setTimeout(() => {
-            this.trigger('g:eventStream.stop');
-            this._eventSource.close();
-            this._eventSource = null;
-        }, this.settings._heartbeatTimeout);
-    } else {
-        this.open();
+    // If _heartbeat was called from a place other than an animation frame, ensure there are no pending calls queued
+    window.cancelAnimationFrame(this._animationRequestID);
+    // Schedule the next heartbeat check
+    // FIXME: This is a recursive call, which is growing the stack on every animation frame!
+    this._animationRequestID = window.requestAnimationFrame(_.bind(this._heartbeat, this));
+
+    // Reset the dead man's switch
+    window.clearTimeout(this._timeoutID);
+    this._timeoutID = window.setTimeout(() => {
+        // If the dead man's switch fires, stop the event stream, but leave it open
+        this._stop();
+        // Note, there is still a pending heartbeat call whenever the page is rendered next
+    }, this.settings._heartbeatTimeout);
+
+    if (!this._eventSource) {
+        // EventStream is open but stopped
+        this._start();
     }
 };
 
-prototype.open = function () {
-    if (window.EventSource) {
-        var stream = this,
-            url = getApiRoot() + this.settings.streamPath,
-            params = {};
-
-        if (this.settings.timeout) {
-            params.timeout = this.settings.timeout;
-        }
-
-        // Set the "since" argument to filter out notifications
-        // that have already been sent to this client.
-        var since;
-        try {
-            since = parseInt(window.localStorage.getItem('sseTimestamp'), 10);
-        } catch (e) {
-            // Ignore any errors raised by localStorage
-        }
-        if (since >= 0) {
-            params.since = since;
-        } else if (_.isNumber(timestamp)) {
-            params.since = timestamp;
-        }
-
-        url += '?' + $.param(params);
-        this._eventSource = new window.EventSource(url);
-
-        this._eventSource.onmessage = function (e) {
-            var obj;
-            try {
-                obj = window.JSON.parse(e.data);
-            } catch (err) {
-                console.error('Invalid JSON from SSE stream: ' + e.data + ',' + err);
-                stream.trigger('g:error', e);
-                return;
-            }
-            timestamp = obj._girderTime;
-            try {
-                window.localStorage.setItem('sseTimestamp', timestamp);
-            } catch (e) {
-                // Ignore any errors raised by localStorage
-            }
-            stream.trigger('g:event.' + obj.type, obj);
-        };
-        this._stopHeartbeat = false;
-        this._heartbeat();
-        stream.trigger('g:eventStream.start');
-    } else {
+EventStream.prototype.open = function () {
+    if (!window.EventSource) {
         console.error('EventSource is not supported on this platform.');
+        return;
     }
+
+    this._opened = true;
+    this._start();
 };
 
-prototype.close = function () {
-    this.trigger('g:eventStream.close');
-    this._stopHeartbeat = true;
+EventStream.prototype._start = function () {
+    const params = {};
+
+    if (this.settings.timeout) {
+        params.timeout = this.settings.timeout;
+    }
+
+    // Set the "since" argument to filter out notifications that have already been sent to this client.
+    const timestamp = EventStream.getLastTimestamp();
+    if (_.isNumber(timestamp)) {
+        params.since = timestamp;
+    }
+
+    const url = getApiRoot() + this.settings.streamPath + '?' + $.param(params);
+    this._eventSource = new window.EventSource(url);
+
+    this._eventSource.onmessage = (e) => {
+        let obj;
+        try {
+            obj = window.JSON.parse(e.data);
+        } catch (err) {
+            console.error('Invalid JSON from SSE stream: ' + e.data + ',' + err);
+            this.trigger('g:error', e);
+            return;
+        }
+        EventStream.setLastTimestamp(obj._girderTime);
+        this.trigger('g:event.' + obj.type, obj);
+    };
+
+    this._heartbeat();
+    this.trigger('g:eventStream.start');
+};
+
+EventStream.prototype._stop = function () {
     if (this._eventSource) {
         this._eventSource.close();
         this._eventSource = null;
     }
+    this.trigger('g:eventStream.stop');
 };
 
-var eventStream = new EventStream();
+EventStream.prototype.close = function () {
+    this._opened = false;
+    this._stop();
+    window.cancelAnimationFrame(this._animationRequestID);
+    window.clearTimeout(this._timeoutID);
+    this.trigger('g:eventStream.close');
+};
+
+// Static methods
+EventStream._lastTimestamp = null;
+EventStream.setLastTimestamp = function (timestamp) {
+    try {
+        window.localStorage.setItem('sseTimestamp', timestamp);
+    } catch (e) {
+        // Ignore any errors raised by localStorage
+    }
+    EventStream._lastTimestamp = timestamp;
+};
+EventStream.getLastTimestamp = function () {
+    let timestamp;
+    try {
+        timestamp = parseInt(window.localStorage.getItem('sseTimestamp'), 10);
+    } catch (e) {
+        // Ignore any errors raised by localStorage
+    }
+    if (!_.isNaN(timestamp)) {
+        // An int was parsed
+        return timestamp;
+    } else {
+        // Value could not be gotten from localStorage, so return from fallback (which is also the default of null)f
+        return EventStream._lastTimestamp;
+    }
+};
+
+const eventStream = new EventStream();
 
 export default eventStream;
