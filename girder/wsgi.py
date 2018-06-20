0c28e5be9f5eaee1f5220930d142908818d2b234
Zach Mullen <zach.mullen@kitware.com>
2018-06-20 16:17:30 -0400

    Add WSGI-compliant log handlers in wsgi.py

--- girder/wsgi.py
+++ girder/wsgi.py
@@ -18,12 +18,23 @@
 ###############################################################################
 
 import cherrypy
+import girder
+import logging.handlers
 from girder.utility import server
 
 cherrypy.config.update({'engine.autoreload.on': False,
                         'environment': 'embedded'})
 cherrypy.config['server'].update({'cherrypy_server': False,
                                   'disable_event_daemon': True})
+
+# TODO The below line can be removed if we do away with girder.logprint
+girder._quiet = True  # This means we won't duplicate messages to stdout/stderr
+_formatter = girder.LogFormatter('[%(asctime)s] %(levelname)s: %(message)s')
+_handler = logging.handlers.StreamHandler()  # Send logs to stderr
+_handler.setFormatter(_formatter)
+girder.logger.addHandler(_handler)
+cherrypy.log.access_log.addHandler(_handler)
+cherrypy.log.error_log.addHandler(_handler)
 
 # 'application' is the default callable object for WSGI implementations, see PEP 3333 for more.
 application = server.setup()
