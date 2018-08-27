3914838c703d1357f00310d28cd07bea0685b832
David Manthey <david.manthey@kitware.com>
2018-08-27 10:41:03 -0400

    Pin to boto3 < 1.8
    Boto3 1.8 breaks our tests using moto.  See
    https://github.com/spulec/moto/issues/1793 .  We can unpin when moto is
    updated.
    
    This also has to add boto-core as a requirement to allow moto to
    install.

--- setup.py
+++ setup.py
@@ -59,7 +59,8 @@ with open('README.rst') as f:
     readme = f.read()
 
 installReqs = [
-    'boto3',
+    'boto3>=1.7,<1.8',
+    'botocore<1.11.0',
     # CherryPy version is restricted due to a bug in versions >=11.1
     # https://github.com/cherrypy/cherrypy/issues/1662
     'CherryPy<11.1',
