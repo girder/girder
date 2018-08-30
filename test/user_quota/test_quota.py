1d722d7d0288162c26741cc97232093985cd5869
Zach Mullen <zach.mullen@kitware.com>
2018-08-29 21:46:25 -0400

    Add test to ensure upload quotas are respected for job artifacts
    More generally, this tests that uploads to things outside the data
    hierarchy are also captured by the user quota policy enforcement
    mechanisms.

---
+++ test/user_quota/test_quota.py
@@ -0,0 +1,33 @@
+import json
+import pytest
+from pytest_girder.assertions import assertStatus, assertStatusOk
+
+
+@pytest.mark.plugin('jobs', 'user_quota')
+def testQuotaEnforcedOutsideHierarchy(server, admin, user, fsAssetstore):
+    from girder.plugins.jobs.models.job import Job
+
+    # Set quota on the user
+    resp = server.request('/user/%s/quota' % user['_id'], method='PUT', user=admin, params={
+        'policy': json.dumps({
+            'fileSizeQuota': 1,
+            'useQuotaDefault': False
+        })
+    })
+    assertStatusOk(resp)
+
+    job = Job().createJob('test', 'test', user=user)
+    url = '/job/%s/artifact' % job['_id']
+    params = {
+        'name': 'foo',
+        'mimeType': 'text/plain',
+        'size': 3
+    }
+
+    resp = server.request(
+        url, method='POST', type='text/plain', body=b'foo', user=user, params=params)
+    assertStatus(resp, 400)
+    assert resp.json['message'] == ('Upload would exceed file storage quota (need 3 B, '
+                                    'only 1 B available - used 0 B out of 1 B)')
+    assert resp.json['type'] == 'validation'
+    assert resp.json['field'] == 'size'
