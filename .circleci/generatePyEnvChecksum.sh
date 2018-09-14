cd8bffc429c8d43dd637b03efe118e2966d5b28e
Jonathan Beezley <jonathan.beezley@kitware.com>
2018-09-14 08:46:33 -0400

    Improve the circleci cache checksum generation

---
+++ .circleci/generatePyEnvChecksum.sh
@@ -0,0 +1,5 @@
+sha512sum /home/circleci/project/girder/setup.py \
+    /home/circleci/project/girder/requirements-dev.txt \
+    /home/circleci/project/girder/pytest_girder/setup.py \
+    /home/circleci/project/girder/plugins/*/setup.py \
+    /home/circleci/project/girder/clients/python/setup.py
