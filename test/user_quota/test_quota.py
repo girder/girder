5aa70f717a19c67308a85695c705fcb78a348ecf
Zach Mullen <zach.mullen@kitware.com>
2018-09-20 17:36:07 -0400

    Add test to verify artifacts count toward quota

--- test/user_quota/test_quota.py
+++ test/user_quota/test_quota.py
@@ -32,3 +32,36 @@ def testQuotaEnforcedOutsideHierarchy(server, admin, user, fsAssetstore):
                                     'only 1 B available - used 0 B out of 1 B)')
     assert resp.json['type'] == 'validation'
     assert resp.json['field'] == 'size'
+
+
+@pytest.mark.plugin('jobs')
+@pytest.mark.plugin('user_quota')
+def testAttachedFilesCountTowardQuota(server, admin, user, fsAssetstore):
+    resp = server.request('/user/%s/quota' % user['_id'], method='PUT', user=admin, params={
+        'policy': json.dumps({
+            'fileSizeQuota': 1,
+            'useQuotaDefault': False
+        })
+    })
+    assertStatusOk(resp)
+
+    job = Job().createJob('test', 'test', user=user)
+    url = '/job/%s/artifact' % job['_id']
+    params = {
+        'name': 'onebyte.txt',
+        'mimeType': 'text/plain',
+        'size': 1
+    }
+    # First should succeed
+    resp = server.request(
+        url, method='POST', type='text/plain', body=b'0', user=user, params=params)
+    assertStatusOk(resp)
+
+    # Second should fail since the first counts toward the quota
+    resp = server.request(
+        url, method='POST', type='text/plain', body=b'0', user=user, params=params)
+    assertStatus(resp, 400)
+    assert resp.json['message'] == ('Upload would exceed file storage quota (need 1 B, '
+                                    'only 0 B available - used 1 B out of 1 B)')
+    assert resp.json['type'] == 'validation'
+    assert resp.json['field'] == 'size'
