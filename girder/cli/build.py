f31b3b7fb7274f27239a361436adc09e05add65d
Jonathan Beezley <jonathan.beezley@kitware.com>
2018-10-05 08:39:27 -0400

    Add an environment variable to configure the npm path
    This replaces the old `--npm` flag that was omitted in the new build
    implementation.

--- girder/cli/build.py
+++ girder/cli/build.py
@@ -21,12 +21,18 @@ import json
 import os
 from pkg_resources import resource_filename
 from subprocess import check_call
+import shutil
 import sys
 
 import click
+import six
 
 from girder.constants import STATIC_ROOT_DIR
 from girder.plugin import allPlugins, getPlugin
+
+# monkey patch shutil for python < 3
+if not six.PY3:
+    import shutilwhich  # noqa
 
 _GIRDER_BUILD_ASSETS_PATH = resource_filename('girder', 'web_client')
 
@@ -41,6 +47,13 @@ _GIRDER_BUILD_ASSETS_PATH = resource_filename('girder', 'web_client')
 @click.option('--reinstall/--no-reinstall', default=True,
               help='Force regenerate node_modules.')
 def main(dev, watch, watch_plugin, reinstall):
+    npmCommand = os.getenv('NPM_EXE', 'npm')
+    if shutil.which(npmCommand) is None:
+        raise click.UsageError(
+            'No npm executable was detected.  Please ensure the npm executable is in your '
+            'path or set the "NPM_EXE" environment variable.'
+        )
+
     if watch and watch_plugin:
         raise click.UsageError('--watch and --watch-plugins cannot be used together')
     if watch or watch_plugin:
@@ -58,11 +71,11 @@ def main(dev, watch, watch_plugin, reinstall):
         if os.path.exists(npmLockFile):
             os.unlink(npmLockFile)
 
-        check_call(['npm', 'install'], cwd=staging)
+        check_call([npmCommand, 'install'], cwd=staging)
 
     quiet = '--no-progress=false' if sys.stdout.isatty() else '--no-progress=true'
     buildCommand = [
-        'npx', 'grunt', '--static-path=%s' % STATIC_ROOT_DIR, quiet]
+        npmCommand, 'run', 'build', '--', '--static-path=%s' % STATIC_ROOT_DIR, quiet]
     if watch:
         buildCommand.append('--watch')
     if watch_plugin:
