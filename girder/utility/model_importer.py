de9ee5a909614ca7b283180c8ea33da6c3e9dc36
Zach Mullen <zach.mullen@kitware.com>
2018-10-18 14:15:52 -0400

    Change behavior of ModelImporter
    Fixes #2377. This removes the ability of the ModelImporter to
    infer the class of a model based on the string(s), instead requiring
    each model to be explicitly registered to a string if it is needed
    for model loading.
    
    This also changes the API for registering models to the ModelImporter;
    rather than registering the model instance, one now only registers the
    model type. This is cleaner in that it can defer model instantiation,
    and also makes the ModelImporter decoupled from the fact that models
    are singletons.
    
    This is a breaking change.

--- girder/utility/model_importer.py
+++ girder/utility/model_importer.py
@@ -17,44 +17,7 @@
 #  limitations under the License.
 ###############################################################################
 
-import importlib
-import six
-
-from . import camelcase
-from girder import logger
-
-# We want the models to essentially be singletons, so we keep this centralized
-# cache of instantiated models that have been lazy-loaded.
-_modelInstances = {}
-
-
-def _loadModel(model, module, plugin):
-    className = camelcase(model)
-
-    try:
-        imported = importlib.import_module(module)
-    except ImportError:
-        logger.exception('Could not load model "%s".' % module)
-        raise
-
-    try:
-        constructor = getattr(imported, className)
-    except AttributeError:
-        raise Exception('Incorrect model class name "%s" for model "%s".' % (
-            className, module))
-
-    _modelInstances[plugin][model] = constructor()
-
-
-def reinitializeAll():
-    """
-    Force all models to reconnect/rebuild indices (needed for testing).
-
-    .. deprecated
-    """
-    for pluginModels in list(six.viewvalues(_modelInstances)):
-        for model in list(six.viewvalues(pluginModels)):
-            model.reconnect()
+_modelClasses = {}
 
 
 class ModelImporter(object):
@@ -63,53 +26,41 @@ class ModelImporter(object):
     should extend/mixin this class.
     """
     @staticmethod
-    def model(model, plugin=None):
+    def model(model, plugin='_core'):
         """
         Call this to get the instance of the specified model. It will be
         lazy-instantiated.
 
-        :param model: The name of the model to get. This is the module
-                      name, e.g. "folder". The class name must be the
-                      upper-camelcased version of that module name, e.g.
-                      "Folder".
+        :param model: The name of the model to get. This must have been
+            registered using the :py:ref
         :type model: string
-        :param plugin: If the model you wish to load is a model within a plugin,
-                       set this to the name of the plugin containing the model.
+        :param plugin: Plugin identifier (if this is a plugin model).
+        :type plugin: str
         :returns: The instantiated model, which is a singleton.
         """
-        if plugin is None:
-            plugin = '_core'
+        if not _modelClasses.get(plugin, {}).get(model):
+            raise Exception('Model "%s.%s" is not registered.' % (plugin, model))
 
-        if plugin not in _modelInstances:
-            _modelInstances[plugin] = {}
-
-        if model not in _modelInstances[plugin]:
-            if plugin == '_core':
-                module = 'girder.models.%s' % model
-            else:
-                raise Exception('Model "%s" is not registered in plugin "%s"' % (model, plugin))
-
-            _loadModel(model, module, plugin)
-
-        return _modelInstances[plugin][model]
+        return _modelClasses[plugin][model]()
 
     @staticmethod
-    def registerModel(model, instance, plugin='_core'):
+    def registerModel(model, cls, plugin='_core'):
         """
-        Use this method to manually register a model singleton instead of
-        having it automatically discovered.
+        Use this method to register a model class to a name. Using this, it can
+        be referenced via the ``model`` method of this class.
 
         :param model: The model name.
         :type model: str
-        :param plugin: If a plugin model, pass the canonical plugin name.
+        :param plugin: Plugin identifier (if this is a plugin model).
         :type plugin: str
-        :param instance: The model singleton instance.
-        :type instance: subclass of Model
+        :param cls: The model class, should be a subclass of
+            :py:class:`girder.models.model_base.Model`.
+        :type cls: type
         """
-        if plugin not in _modelInstances:
-            _modelInstances[plugin] = {}
-        _modelInstances[plugin][model] = instance
+        if plugin not in _modelClasses:
+            _modelClasses[plugin] = {}
+        _modelClasses[plugin][model] = cls
 
     @staticmethod
     def unregisterModel(model, plugin='_core'):
-        del _modelInstances[plugin][model]
+        del _modelClasses[plugin][model]
