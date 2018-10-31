2a2f7b5d41bcb28bd44683a0bfb2e77c8b3389db
Zach Mullen <zach.mullen@kitware.com>
2018-10-31 13:55:06 -0400

    Create a UID for every request and include it in error logs

--- test/test_rest_exception_handling.py
+++ test/test_rest_exception_handling.py
@@ -1,4 +1,6 @@
+import cherrypy
 import contextlib
+import mock
 import pytest
 from girder import config
 from girder.api import access
@@ -18,20 +20,31 @@ def serverMode(mode):
 def exceptionServer(server):
     @access.public
     def _raiseException(*args, **kwargs):
-        raise Exception('Specific message')
+        raise Exception('Specific message ' + cherrypy.request.girderRequestUid)
 
     server.root.api.v1.item.route('GET', ('exception',), _raiseException)
     yield server
     server.root.api.v1.item.removeRoute('GET', ('exception',))
 
 
+@pytest.fixture
+def uuidMock():
+    val = '1234'
+    with mock.patch('uuid.uuid4', return_value=val):
+        yield val
+
+
 @pytest.mark.parametrize('mode,msg,hasTrace', [
     ('production', 'An unexpected error occurred on the server.', False),
-    ('development', 'Exception: Exception(\'Specific message\',)', True)
+    ('development', 'Exception: Exception(\'Specific message 1234\',)', True)
 ])
-def testExceptionHandlingBasedOnServerMode(exceptionServer, mode, msg, hasTrace):
+def testExceptionHandlingBasedOnServerMode(exceptionServer, uuidMock, mode, msg, hasTrace):
     with serverMode(mode):
         resp = exceptionServer.request('/item/exception', exception=True)
-        assertStatus(resp, 500)
-        assert resp.json['message'] == msg
-        assert ('trace' in resp.json) is hasTrace
+
+    assertStatus(resp, 500)
+    assert resp.json['message'] == msg
+    assert resp.json['type'] == 'internal'
+    assert resp.json['uid'] == uuidMock
+    assert ('trace' in resp.json) is hasTrace
+    assert resp.headers['Girder-Request-Uid'] == uuidMock
