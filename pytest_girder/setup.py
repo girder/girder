c928219bab1a9db0517274d0e13ae094d57ad7e4
Dan LaManna <dan.lamanna@kitware.com>
2018-11-06 09:10:11 -0500

    Add the mock dependency to pytest-girder

--- pytest_girder/setup.py
+++ pytest_girder/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='pytest-girder',
-    version='0.1.0a1',
+    version='0.1.0a2',
     description='A set of pytest fixtures for testing Girder applications.',
     author='Kitware, Inc.',
     author_email='kitware@kitware.com',
@@ -13,6 +13,7 @@ setup(
     packages=find_packages(),
     install_requires=[
         'girder>=3.0.0a1',
+        'mock',
         'mongomock',
         'pytest>=3.6',
         'pytest-cov<2.6',
