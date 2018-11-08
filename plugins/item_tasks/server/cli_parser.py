68024c2797c70ac7de4d60e226f58eb9e795035c
Jonathan Beezley <jonathan.beezley@kitware.com>
2018-11-08 08:02:13 -0500

    Support directory outputs from slicer cli's

--- plugins/item_tasks/server/cli_parser.py
+++ plugins/item_tasks/server/cli_parser.py
@@ -27,7 +27,8 @@ _SLICER_TO_GIRDER_WORKER_INPUT_TYPE_MAP = {
 _SLICER_TO_GIRDER_WORKER_OUTPUT_TYPE_MAP = {
     'file': 'new-file',
     'image': 'new-file',
-    'pointfile': 'new-file'
+    'pointfile': 'new-file',
+    'directory': 'new-folder'
 }
 
 _SLICER_TYPE_TO_GIRDER_MODEL_MAP = {
