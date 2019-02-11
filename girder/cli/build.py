628db1d07832f4153738c5bfeb5548aa7b38d3a0
David Manthey <david.manthey@kitware.com>
2019-02-11 12:52:13 -0500

    Add underscore and jquery to package.json.template.
    Revert changing the web-build path.

--- girder/cli/build.py
+++ girder/cli/build.py
@@ -35,7 +35,7 @@ from girder.utility.server import getStaticRoot
 if not six.PY3:
     import shutilwhich  # noqa
 
-_GIRDER_BUILD_ASSETS_PATH = os.path.realpath(resource_filename('girder', 'web_client'))
+_GIRDER_BUILD_ASSETS_PATH = resource_filename('girder', 'web_client')
 
 
 @click.command(name='build', help='Build web client static assets.')
