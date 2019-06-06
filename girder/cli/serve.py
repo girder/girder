b2e6c1ad0473df07f50f2dd474c91938fcf30357
Jake Nesbitt <jjnesbitt2@gmail.com>
2019-06-06 16:00:26 -0400

    Add --dev option to girder serve command

--- girder/cli/serve.py
+++ girder/cli/serve.py
@@ -8,6 +8,7 @@ from girder.utility import server
 
 
 @click.command(name='serve', short_help='Run the Girder server.', help='Run the Girder server.')
+@click.option('--dev', is_flag=True, help='Run in development mode')
 @click.option('-t', '--testing', is_flag=True, help='Run in testing mode')
 @click.option('-d', '--database', default=cherrypy.config['database']['uri'],
               show_default=True, help='The database URI to connect to')
@@ -15,7 +16,7 @@ from girder.utility import server
               show_default=True, help='The interface to bind to')
 @click.option('-p', '--port', type=int, default=cherrypy.config['server.socket_port'],
               show_default=True, help='The port to bind to')
-def main(testing, database, host, port):
+def main(dev, testing, database, host, port):
     # If the user provides no options, the existing config values get re-set through click
     cherrypy.config['database']['uri'] = database
     if six.PY2:
@@ -27,7 +28,7 @@ def main(testing, database, host, port):
     cherrypy.config['server.socket_port'] = port
 
     _attachFileLogHandlers()
-    server.setup(testing)
+    server.setup(dev, testing)
 
     cherrypy.engine.start()
     cherrypy.engine.block()
