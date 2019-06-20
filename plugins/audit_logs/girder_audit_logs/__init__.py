068a5936510de931e1333670aaaf8de0f8bbd7f7
Brian Helba <brian.helba@kitware.com>
2019-06-20 13:38:37 -0400

    Guard against inserting fields with null-character strings into MongoDB

--- plugins/audit_logs/girder_audit_logs/__init__.py
+++ plugins/audit_logs/girder_audit_logs/__init__.py
@@ -1,6 +1,7 @@
 import cherrypy
 import datetime
 import logging
+import six
 from girder import auditLogger
 from girder.models.model_base import Model
 from girder.api.rest import getCurrentUser
@@ -18,6 +19,19 @@ class Record(Model):
 class _AuditLogDatabaseHandler(logging.Handler):
     def handle(self, record):
         user = getCurrentUser()
+
+        # Null characters may not be stored as MongoDB Object keys
+        # RFC3986 technically allows such characters to be encoded in the query string, and 'params'
+        # also contains data from form bodies, which may contain arbitrary field names
+        if record.msg == 'rest.request' and any(
+            '\x00' in paramKey
+            for paramKey in six.viewkeys(record.details['params'])
+        ):
+            record.details['params'] = {
+                paramKey.replace('\x00', ''): paramValue
+                for paramKey, paramValue in six.viewitems(record.details['params'])
+            }
+
         Record().save({
             'type': record.msg,
             'details': record.details,
