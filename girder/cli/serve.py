0b2118fd7d7999081c9d54d30223b2f06147d8da
Jake Nesbitt <jjnesbitt2@gmail.com>
2019-06-27 11:37:37 -0400

    Add back --dev flag to build and serve commands

--- girder/cli/serve.py
+++ girder/cli/serve.py
@@ -9,6 +9,7 @@ from girder.constants import PRODUCTION_MODE, DEVELOPMENT_MODE, TESTING_MODE
 
 
 @click.command(name='serve', short_help='Run the Girder server.', help='Run the Girder server.')
+@click.option('--dev', default=False, is_flag=True, help='Sets the server mode to development')
 @click.option('--mode', type=click.Choice([PRODUCTION_MODE, DEVELOPMENT_MODE, TESTING_MODE]),
               default=PRODUCTION_MODE, show_default=True, help='Specify the server mode')
 @click.option('-d', '--database', default=cherrypy.config['database']['uri'],
@@ -17,7 +18,10 @@ from girder.constants import PRODUCTION_MODE, DEVELOPMENT_MODE, TESTING_MODE
               show_default=True, help='The interface to bind to')
 @click.option('-p', '--port', type=int, default=cherrypy.config['server.socket_port'],
               show_default=True, help='The port to bind to')
-def main(mode, database, host, port):
+def main(dev, mode, database, host, port):
+    if (dev):
+        mode = DEVELOPMENT_MODE
+
     # If the user provides no options, the existing config values get re-set through click
     cherrypy.config['database']['uri'] = database
     if six.PY2:
