d832a86c9e5357075418f2edc5102e874214751c
Jeffrey Baumes <jeff.baumes@kitware.com>
2023-05-31 13:00:25 -0400

    Remove some spurious config options

--- girder/web_client/vitest.config.ts
+++ girder/web_client/vitest.config.ts
@@ -7,20 +7,11 @@ export default mergeConfig(
   viteConfig,
   defineConfig({
     test: {
-      environment: 'jsdom',
-      // exclude: [...configDefaults.exclude, 'e2e/*'],
       exclude: [...configDefaults.exclude],
       root: fileURLToPath(new URL('./', import.meta.url)),
       transformMode: {
         web: [/\.[jt]sx$/],
       },
-      coverage: {
-        provider: 'istanbul',
-      },
-      // browser: {
-      //   enabled: true,
-      //   name: 'chrome',
-      // },
     },
   })
 )
