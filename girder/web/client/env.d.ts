79271d87a46da218f2bb98fdceff5afad25d1727
Jeffrey Baumes <jeff.baumes@kitware.com>
2023-07-05 10:07:17 -0400

    Get npm build working for client

--- girder/web/client/env.d.ts
+++ girder/web/client/env.d.ts
@@ -1,1 +1,5 @@
 /// <reference types="vite/client" />
+
+declare module 'jquery';
+declare module 'underscore';
+declare module 'moment';
--- girder/web/client/src/main.ts
+++ girder/web/client/src/main.ts
@@ -8,9 +8,12 @@ declare global {
 }
 window.girder = girder;
 
-import.meta.glob('/node_modules/**/girder-plugin-*/dist/*.css', { eager: true });
-const packageInfo = import.meta.glob('/node_modules/**/girder-plugin-*/package.json', { eager: true });
-const modules = import.meta.glob('/node_modules/**/girder-plugin-*/dist/*.js');
-await girder.loadPlugins(modules, packageInfo as Record<string, GirderPackageInfo>);
+const init = async () => {
+  import.meta.glob('/node_modules/**/girder-plugin-*/dist/*.css', { eager: true });
+  const packageInfo = import.meta.glob('/node_modules/**/girder-plugin-*/package.json', { eager: true });
+  const modules = import.meta.glob('/node_modules/**/girder-plugin-*/dist/*.js');
+  await girder.loadPlugins(modules, packageInfo as Record<string, GirderPackageInfo>);
+  girder.initializeDefaultApp();
+};
 
-girder.initializeDefaultApp();
+init();
