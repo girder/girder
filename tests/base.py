fecafb9c8a31b10c64cc1a1f3a45ae0bf6aff262
Zach Mullen <zach.mullen@kitware.com>
2023-10-06 10:31:09 -0400

    Fix app prefix in legacy CTest tests

--- tests/base.py
+++ tests/base.py
@@ -433,7 +433,7 @@ class TestCase(unittest.TestCase):
                 prefix='/api/v1', isJson=True, basicAuth=None, body=None,
                 type=None, exception=False, cookie=None, token=None,
                 additionalHeaders=None, useHttps=False,
-                authHeader='Authorization', appPrefix=''):
+                authHeader='Authorization', appPrefix='/api'):
         """
         Make an HTTP request.
 
