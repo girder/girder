47dd6c621e208100dd745e8eef425bdc4e23f64d
David Manthey <david.manthey@kitware.com>
2023-12-05 11:34:15 -0500

    Mark auth cookie as httponly

--- girder/api/rest.py
+++ girder/api/rest.py
@@ -1181,6 +1181,7 @@ class Resource:
         cookie['girderToken'] = str(token['_id'])
         cookie['girderToken']['path'] = '/'
         cookie['girderToken']['expires'] = int(days * 3600 * 24)
+        cookie['girderToken']['httponly'] = True
 
         # CherryPy proxy tools modify the request.base, but not request.scheme, when receiving
         # X-Forwarded-Proto headers from a reverse proxy
