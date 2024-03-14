c0e2521e5edcc747081ba5ecb3a8697c79697150
David Manthey <david.manthey@kitware.com>
2024-03-14 07:38:26 -0400

    Fix passing integer values to diskcache options

--- girder/cli/mount.py
+++ girder/cli/mount.py
@@ -96,6 +96,8 @@ class ServerFuse(fuse.Operations):
         for key in list((cacheopts or {}).keys()):
             if key.startswith('diskcache'):
                 value = cacheopts.pop(key)
+                if value.strip().isdigit():
+                    value = int(value)
                 key = key[len('diskcache'):].lstrip('_')
                 use = True if use is None else use
                 if key:
