69f36c01a2ee992886d7e800304bffaaa92bfbc9
Zach Mullen <zach.mullen@kitware.com>
2024-11-07 09:49:42 -0500

    Fix imports in test

--- test/worker/test_girder_plugin.py
+++ test/worker/test_girder_plugin.py
@@ -1,21 +1,3 @@
-#!/usr/bin/env python
-
-###############################################################################
-#  Copyright Kitware Inc.
-#
-#  Licensed under the Apache License, Version 2.0 ( the "License" );
-#  you may not use this file except in compliance with the License.
-#  You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
-#  limitations under the License.
-###############################################################################
-
 import io
 import json
 from unittest import mock
@@ -29,9 +11,9 @@ from girder_jobs.models.job import Job
 from girder_jobs.constants import JobStatus
 from pytest_girder.assertions import assertStatus, assertStatusOk
 
-from girder_worker.girder_plugin.constants import PluginSettings
-from girder_worker.girder_plugin import celery, utils
-from girder_worker.girder_plugin.status import CustomJobStatus
+from girder_plugin_worker.constants import PluginSettings
+from girder_plugin_worker import celery, utils
+from girder_plugin_worker.status import CustomJobStatus
 
 
 def tearDownModule():
