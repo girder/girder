3fb92ab3846e88ec5049bc7fdf9831699efebec2
Bryon Lewis <Bryon.Lewis@kitware.com>
2025-11-12 13:53:32 -0500

    feat: set plugin path when base URL is different

--- girder/web/src/main.ts
+++ girder/web/src/main.ts
@@ -13,6 +13,7 @@ interface StaticFilesSpec {
 }
 
 let apiRoot = import.meta.env.VITE_API_ROOT;
+let pluginRoot = import.meta.env.VITE_GIRDER_PLUGIN_ROOT;
 
 (async () => {
   if (!apiRoot) {
@@ -27,13 +28,30 @@ let apiRoot = import.meta.env.VITE_API_ROOT;
   const staticFilesResp = await fetch(`${apiRoot}/system/plugin_static_files`);
   const staticFiles: StaticFilesSpec = await staticFilesResp.json();
 
+  // The plugin root defaults to the location of the client app
+  // That is correct for most deployments but if girder is being served from a different base path compared to the api
+  // then the plugin root must be set explicitly via an environment variable at build time.
+  if (!pluginRoot) {
+    // Check and see if the api root matches the location of the client app
+    const appElement = document.getElementById('app');
+    const baseApiPath = (appElement && appElement.getAttribute('root')) || '';
+    const baseAppPath = window.location.href.replace(window.location.origin, '');
+    if (baseAppPath.includes(baseApiPath)) {
+      // Adjust the plugin root to use the same base path as the api root
+      pluginRoot = window.location.origin + baseApiPath;
+    }
+  }
+
+
   staticFiles.css.forEach((href) => {
     const link = document.createElement('link');
     link.rel = 'stylesheet';
     link.type = 'text/css';
-    link.href = new URL(href, window.location.href).href;
+    link.href = new URL(href, pluginRoot).href;
     document.head.appendChild(link);
   });
+
+
 
   // Since plugin JS files may implicitly depend on each other at import time, we can't load
   // them in parallel. They already come to us in topoligically sorted order, so we can safely
@@ -42,7 +60,7 @@ let apiRoot = import.meta.env.VITE_API_ROOT;
     await new Promise<void>((resolve) => {
       const script = document.createElement('script');
       script.type = 'text/javascript';
-      script.src = new URL(href, window.location.href).href;
+      script.src = new URL(href, pluginRoot).href;
       document.head.appendChild(script);
       script.addEventListener('load', function() {
         resolve();
