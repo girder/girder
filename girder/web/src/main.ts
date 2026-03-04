e54d3eea677a08cc88e462fb432b03c4f7539b05
Bryon Lewis <Bryon.Lewis@kitware.com>
2026-03-04 08:03:17 -0500

    add missing trailing slash to apiRoot

--- girder/web/src/main.ts
+++ girder/web/src/main.ts
@@ -30,7 +30,7 @@ let apiRoot = import.meta.env.VITE_API_ROOT;
   // The plugin root defaults to root location of the api
   // That is correct for most deployments but if girder is being served from a different base path compared to the api
   // then the plugin root must be modified to match the api location
-  const pluginRoot = apiRoot.indexOf(':') >= 0 ? apiRoot.replace(/\/api\/v1\/?$/, '') : window.location.origin + apiRoot.replace(/\/api\/v1\/?$/, '');
+  const pluginRoot = apiRoot.indexOf(':') >= 0 ? apiRoot.replace(/\/api\/v1\/?$/, '/') : window.location.origin + apiRoot.replace(/\/api\/v1\/?$/, '/');
 
   staticFiles.css.forEach((href) => {
     const link = document.createElement('link');
