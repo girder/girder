ea63252f5f7bdd1ebc8ac03b47cf24f39a1aa07e
David Manthey <david.manthey@kitware.com>
2026-05-15 13:10:04 -0400

    When uploading, add a default mime type

---
+++ girder/web/tests/spec/file_upload.spec.ts
@@ -0,0 +1,22 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+import { test } from '@playwright/test';
+
+import { setupServer } from '../server';
+import { createUser, upload } from '../util';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+test.describe('Test file endpoints from the front-end', () => {
+    setupServer();
+
+    test('file upload uses default mime type when none provided', async ({ page }) => {
+        await createUser(page, 'admin');
+        await page.locator('#g-app-header-container').getByText('admin').click();
+        await page.getByRole('link', { name: ' My folders' }).click();
+        await page.getByRole('link', { name: ' Private ' }).click();
+        await upload(page, path.join(__dirname, 'data', 'nomime.dat'));
+    });
+});
