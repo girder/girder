20465ab595441e8a124baa7f6da8d63b52ebaaab
Mary Salvi <mary.salvi@kitware.com>
2026-06-09 13:03:18 -0400

    Use update for config changes to preserve immutability for rtd

--- girder/utility/config.py
+++ girder/utility/config.py
@@ -9,20 +9,20 @@ logger = logging.getLogger(__name__)
 def loadConfig():
     # Calling this has the side effect of populating the cherrypy.config object with
     # values from the environment. It is called at import time of the girder package.
-    cherrypy.config['server.socket_host'] = os.getenv('GIRDER_HOST', '127.0.0.1')
-    cherrypy.config['server.socket_port'] = int(os.getenv('GIRDER_PORT', 8080))
-    cherrypy.config['server.thread_pool'] = int(os.getenv('GIRDER_THREAD_POOL', 100))
-    cherrypy.config['tools.proxy.on'] = True
+    cherrypy.config.update({'server.socket_host': os.getenv('GIRDER_HOST', '127.0.0.1')})
+    cherrypy.config.update({'server.socket_port': int(os.getenv('GIRDER_PORT', 8080))})
+    cherrypy.config.update({'server.thread_pool': int(os.getenv('GIRDER_THREAD_POOL', 100))})
+    cherrypy.config.update({'tools.proxy.on': True})
 
     if 'database' not in cherrypy.config:
-        cherrypy.config['database'] = {}
+        cherrypy.config.update({'database': {}})
 
     if 'GIRDER_TEST_DB' in os.environ:
-        cherrypy.config['database']['uri'] = os.environ['GIRDER_TEST_DB'].replace('.', '_')
+        cherrypy.config['database'].update({'uri': os.environ['GIRDER_TEST_DB'].replace('.', '_')})
     else:
-        cherrypy.config['database']['uri'] = os.getenv(
-            'GIRDER_MONGO_URI', 'mongodb://localhost:27017/girder')
-        cherrypy.config['database']['replica_set'] = os.getenv('GIRDER_MONGO_REPLICA_SET')
+        cherrypy.config['database'].update({'uri': os.getenv(
+            'GIRDER_MONGO_URI', 'mongodb://localhost:27017/girder')})
+        cherrypy.config['database'].update({'replica_set': os.getenv('GIRDER_MONGO_REPLICA_SET')})
 
 
 def getConfig():
