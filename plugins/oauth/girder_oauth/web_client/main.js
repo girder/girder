7a1d3d8d3e69b2678dfc02590fd6e402b18405dd
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2026-06-11 10:45:15 -0500

    Propagate policy errors in OAuth2 flow
    If girder user account is disabled or require an additional action
    before allowing user to login, inject meaningful 'error' in OAuth
    callback redirect.
    
    Fixes #3838

--- plugins/oauth/girder_oauth/web_client/main.js
+++ plugins/oauth/girder_oauth/web_client/main.js
@@ -4,6 +4,8 @@ import './routes';
 import './views/LoginView';
 import './views/RegisterView';
 
+const events = girder.events;
+
 // If the current URL contains a `girderToken` query parameter, set the current token to its value
 const girderToken = new URLSearchParams(window.location.search).get('girderToken');
 
@@ -17,3 +19,28 @@ if (girderToken) {
     queryParams.delete('girderToken');
     window.location.search = queryParams.toString();
 }
+
+const error = new URLSearchParams(window.location.search).get('error');
+if (error) {
+    window.localStorage.setItem('oauthError', error);
+    const queryParams = new URLSearchParams(window.location.search);
+    queryParams.delete('error');
+    window.location.search = queryParams.toString();
+}
+
+events.on('g:appload.after', function() {
+    const error = window.localStorage.getItem('oauthError');
+    if (error) {
+        var alertText = `OAuth login failed: ${error}`;
+        if (error === 'accountApproval') {
+            alertText = 'Your account is pending approval by an administrator.';
+        }
+        setTimeout(() => {
+            events.trigger('g:alert', {
+                text: alertText,
+                type: 'danger',
+            });
+        }, 100);
+        window.localStorage.removeItem('oauthError');
+    };
+});
